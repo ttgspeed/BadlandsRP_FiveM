1a65b38751e1a9719542474d74910f6e1ef131ee
Ozadu <normpad48@gmail.com>
2017-06-01 15:53:20 -0400

    Revert "ignore server log and remove mystery &"
    This reverts commit b52870e504e65a83dd7017ff044ae11013a55454.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 fx_root=`dirname "$(readlink -f "$0")"`
 
-MONO_PATH=$fx_root/mono/ mono $fx_root/CitizenMP.Server.exe $* | tee -a server.log
+MONO_PATH=$fx_root/mono/ mono $fx_root/CitizenMP.Server.exe $* |& tee -a server.log
