1031782fb4d38d29f58262a553f296bf402a62a0
ttgspeed <ttg.speed@gmail.com>
2017-06-01 23:08:54 -0400

    Remove server cache on startup

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 fx_root=`dirname "$(readlink -f "$0")"`
-
+echo "Removing Cache"
+rm -rf $fx_root/cache
 MONO_PATH=$fx_root/mono/ mono $fx_root/CitizenMP.Server.exe $* |& tee -a server.log
