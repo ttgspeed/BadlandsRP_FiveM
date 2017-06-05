8d6ab37049ef6a4b38721948994253a018903896
Serpico <bamboozol@gmail.com>
2017-06-05 00:52:56 -0400

    Added drug fields, processors and dealers

--- resources/vrp/cfg/item_transformers.lua
+++ resources/vrp/cfg/item_transformers.lua
@@ -24,7 +24,7 @@ cfg.item_transformers = {
     }
   },
   {
-    name="Peaches",
+    name="Peache Field",
     r=255,g=125,b=24,
     max_units=10,
     units_per_minute=1,
@@ -40,7 +40,7 @@ cfg.item_transformers = {
     }
   },
   {
-    name="Peaches",
+    name="Peache Field",
     r=255,g=125,b=24,
     max_units=10,
     units_per_minute=1,
@@ -56,7 +56,7 @@ cfg.item_transformers = {
     }
   },
   {
-    name="Peaches",
+    name="Peache Field",
     r=255,g=125,b=24,
     max_units=10,
     units_per_minute=1,
@@ -153,6 +153,246 @@ cfg.item_transformers = {
     out_money=1000,
     reagents={
       ["gold_ingot"] = 1
+    },
+    products={}
+  },
+  {
+    name="Marijuna Field",
+    r=255,g=255,b=0,
+    max_units=60,
+    units_per_minute=3,
+    x=2213.0224609375,y=5577.65380859375,z=52.7998313903809,
+    radius=8,height=1.5,
+    action="Harvest Marijuna",
+    description="Harvest Marijuna",
+    in_money=0,
+    out_money=0,
+    reagents={},
+    products={
+      ["marijuna"] = 1
+    }
+  },
+  {
+    name="Weed Processor",
+    r=255,g=255,b=0,
+    max_units=1000,
+    units_per_minute=1000,
+    x=-1840.15637207031,y=2152.9907226563,z=115.324966430664,
+    radius=8,height=1.5,
+    action="Process Marijuna",
+    description="Process 1 marijuna into 1 weed.",
+    in_money=0,
+    out_money=0,
+    reagents={ ["marijuna"] = 1},
+    products={
+      ["weed"] = 1
+    }
+  },
+  {
+    name="Ephedrine Field",
+    r=255,g=255,b=0,
+    max_units=60,
+    units_per_minute=3,
+    x=65.3316345214844,y=3716.21728515625,z=38.754467010498,
+    radius=8,height=1.5,
+    action="Harvest Ephedrine",
+    description="Harvest Ephedrine.",
+    in_money=0,
+    out_money=0,
+    reagents={},
+    products={
+      ["ephedrine"] = 1
+    }
+  },
+  {
+    name="Meth Processing Lab",
+    r=255,g=255,b=0,
+    max_units=1000,
+    units_per_minute=1000,
+    x=1391.11328125,y=3608.18896484375,z=37.94189453125,
+    radius=8,height=1.5,
+    action="Process Ephedrine",
+    description="Process 1 ephedrine into 1 meth.",
+    in_money=0,
+    out_money=0,
+    reagents={["ephedrine"] = 1},
+    products={
+      ["meth"] = 1
+    }
+  },
+  {
+    name="Diethylamine Field",
+    r=255,g=255,b=0,
+    max_units=60,
+    units_per_minute=3,
+    x=166.024078369141,y=2229.79077148438,z=89.7329788208008,
+    radius=8,height=1.5,
+    action="Harvest Diethylamine",
+    description="Harvest Diethylamine.",
+    in_money=0,
+    out_money=0,
+    reagents={},
+    products={
+      ["diethylamine"] = 1
+    }
+  },
+  {
+    name="LSD Processing Lab",
+    r=255,g=255,b=0,
+    max_units=1000,
+    units_per_minute=1000,
+    x=2515.3354492187,y=3792.3996582031,z=52.1224937438965,
+    radius=8,height=1.5,
+    action="Process Diethylamine",
+    description="Process 1 diethylamine into 1 LSD.",
+    in_money=0,
+    out_money=0,
+    reagents={["diethylamine"] = 1},
+    products={
+      ["lsd"] = 1
+    }
+  },
+  {
+    name="Safrole Field",
+    r=255,g=255,b=0,
+    max_units=60,
+    units_per_minute=3,
+    x=3856.02709960938,y=4459.1904296875,z=0.84976637363434,
+    radius=8,height=1.5,
+    action="Harvest Safrole",
+    description="Harvest Safrole.",
+    in_money=0,
+    out_money=0,
+    reagents={},
+    products={
+      ["safrole"] = 1
+    }
+  },
+  {
+    name="MDMA Processing Lab",
+    r=255,g=255,b=0,
+    max_units=1000,
+    units_per_minute=1000,
+    x=-1145.96435546875,y=4940.06689453125,z=221.268676757813,
+    radius=8,height=1.5,
+    action="Process Safrole",
+    description="Process 1 safrole into 1 MDMA.",
+    in_money=0,
+    out_money=0,
+    reagents={["safrole"] = 1},
+    products={
+      ["mdma"] = 1
+    }
+  },
+  {
+    name="Cocaine Field",
+    r=255,g=255,b=0,
+    max_units=60,
+    units_per_minute=3,
+    x=3856.02709960938,y=4459.1904296875,z=0.84976637363434,
+    radius=8,height=1.5,
+    action="Harvest Cocaine",
+    description="Harvest Cocaine.",
+    in_money=0,
+    out_money=0,
+    reagents={},
+    products={
+      ["raw_cocaine"] = 1
+    }
+  },
+  {
+    name="Cocaine Processing Lab",
+    r=255,g=255,b=0,
+    max_units=1000,
+    units_per_minute=1000,
+    x=2434.9562988281,y=4968.6137695312,z=41.3476028442383,
+    radius=8,height=1.5,
+    action="Process Cocaine",
+    description="Process 1 raw cocaine into 1 cocaine.",
+    in_money=0,
+    out_money=0,
+    reagents={["raw_cocaine"] = 1},
+    products={
+      ["cocaine"] = 1
+    }
+  },
+  {
+    name="Weed Dealer",
+    r=255,g=125,b=24,
+    max_units=1000,
+    units_per_minute=1000,
+    x=77.885513305664,y=-1948.2086181641,z=19.174139022827,
+    radius=5, height=2.5,
+    action="Sell",
+    description="Sell weed. 25$",
+    in_money=0,
+    out_money=25,
+    reagents = {
+      ["weed"] = 1
+    },
+    products={}
+  },
+  {
+    name="Cocaine Dealer",
+    r=255,g=125,b=24,
+    max_units=1000,
+    units_per_minute=1000,
+    x=-224.3656463623,y=-224.3656463623,z=35.636913299561,
+    radius=5, height=2.5,
+    action="Sell",
+    description="Sell cocaine. 25$",
+    in_money=0,
+    out_money=25,
+    reagents = {
+      ["cocaine"] = 1
+    },
+    products={}
+  },
+  {
+    name="Meth Dealer",
+    r=255,g=125,b=24,
+    max_units=1000,
+    units_per_minute=1000,
+    x=1177.1647949219,y=2722.220703125,z=37.004173278809,
+    radius=5, height=2.5,
+    action="Sell",
+    description="Sell meth. 25$",
+    in_money=0,
+    out_money=25,
+    reagents = {
+      ["meth"] = 1
+    },
+    products={}
+  },
+  {
+    name="LSD Dealer",
+    r=255,g=125,b=24,
+    max_units=1000,
+    units_per_minute=1000,
+    x=-1724.7882080078,y=234.66094970703,z=57.471710205078,
+    radius=5, height=2.5,
+    action="Sell",
+    description="Sell LSD. 25$",
+    in_money=0,
+    out_money=25,
+    reagents = {
+      ["lsd"] = 1
+    },
+    products={}
+  },
+  {
+    name="MDMA Dealer",
+    r=255,g=125,b=24,
+    max_units=1000,
+    units_per_minute=1000,
+    x=1302.6696777344,y=4226.1025390625,z=31.908679962158,
+    radius=5, height=2.5,
+    action="Sell",
+    description="Sell MDMA. 25$",
+    in_money=0,
+    out_money=25,
+    reagents = {
+      ["mdma"] = 1
     },
     products={}
   }
