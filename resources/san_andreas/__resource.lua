65e657d76ebb0096c542508285108a43a67efec2
Serpico <bamboozol@gmail.com>
2017-06-05 15:53:00 -0400

    No longer used

--- resources/san_andreas/__resource.lua
+++
@@ -1,3 +0,0 @@
-resource_type 'map' { name = 'San Andreas', gameTypes = { es_roleplay = true } }
-
-map 'map.lua'
--- resources/san_andreas/map.lua
+++
@@ -1,1 +0,0 @@
-spawnpoint 'a_m_y_skater_02' { x = 1788.25, y = 3890.34, z = 34.3849 }
