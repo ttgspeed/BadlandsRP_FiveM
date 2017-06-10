2720499ffdcf7a5b214ce6606cc4d25f5848a6e1
Serpico <bamboozol@gmail.com>
2017-06-10 12:13:02 -0400

    brought back pnotify. The current notifications are limited to 2 active and it sucks. I'll convert the rest shortly. Also cleaned up paycheck

--- resources/vrp/modules/paycheck.lua
+++ resources/vrp/modules/paycheck.lua
@@ -1,16 +1,22 @@
+local civPay = 800
+local copPay = 1200
+local medicPay = 1200
+local paycheck = 0
+
 RegisterServerEvent('vRP:salary')
 AddEventHandler('vRP:salary', function()
   	local user_id = vRP.getUserId(source)
   	if user_id ~= nil then
 		if vRP.hasPermission(user_id,"police.paycheck") then
-			vRP.giveMoney(user_id,1200)
-			vRPclient.notify(source,{"PAYDAY: $1200"})
+			paycheck = copPay
 		elseif vRP.hasPermission(user_id,"emergency.paycheck") then
-			vRP.giveMoney(user_id,1200)
-			vRPclient.notify(source,{"PAYDAY: $1200"})
+			paycheck = medicPay
 		elseif vRP.hasPermission(user_id,"citizen.paycheck") then
-			vRP.giveMoney(user_id,800)
-			vRPclient.notify(source,{"PAYDAY: $800"})
+			paycheck = civPay
+		else
+			paycheck = 0
 		end
+		vRP.giveMoney(user_id,paycheck)
+		TriggerClientEvent("pNotify:SendNotification", -1, {text = "You received your paycheck of $"..paycheck.."." , type = "success", layout = "centerLeft", queue = "left", timeout = math.random(1000, 10000)})
 	end
 end)
