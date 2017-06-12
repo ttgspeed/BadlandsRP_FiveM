a79884e4096937def1fd201f6a11224919027eb9
Ozadu <normpad48@gmail.com>
2017-06-12 06:29:43 -0400

    Cigarette item

--- resources/vrp/cfg/item/drugs.lua
+++ resources/vrp/cfg/item/drugs.lua
@@ -11,6 +11,27 @@ local function play_drink(player)
   vRPclient.playAnim(player,{true,seq,false})
 end
 
+local function smoke(player)
+	local seq = {
+		--{"amb@world_human_aa_smoke@male@base","base",1},
+		--{"amb@world_human_aa_smoke@male@idle_a","idle_a",1},
+		--{"amb@world_human_aa_smoke@male@idle_a","idle_b",1},
+		--{"amb@world_human_aa_smoke@male@idle_a","idle_c",1},
+		
+		{"amb@world_human_smoking@male@male_a@enter","enter",1},		
+		--{"amb@world_human_smoking@male@male_a@idle_a","idle_a",1},
+		{"amb@world_human_smoking@male@male_a@base","base",5},
+		--{"amb@world_human_smoking@male@male_a@idle_a","idle_b",1},
+		--{"amb@world_human_smoking@male@male_a@idle_a","idle_c",1},
+		{"amb@world_human_smoking@male@male_a@exit","exit",1},		
+	}
+	vRPclient.attachProp(player,{'prop_cs_ciggy_01',28422,0,0,0,0,0,0})
+	vRPclient.playAnim(player,{true,seq,false})
+	SetTimeout(60*1000,function()
+		vRPclient.deleteProp(player,{'prop_cs_ciggy_01'})
+	end)
+end
+
 local pills_choices = {}
 pills_choices["Take"] = {function(player,choice)
   local user_id = vRP.getUserId(player)
@@ -24,6 +45,19 @@ pills_choices["Take"] = {function(player,choice)
   end
 end}
 
+local cig_choices = {}
+cig_choices["Smoke"] = {function(player,choice)
+	local user_id = vRP.getUserId(player)
+	if user_id ~= nil then 
+		if vRP.tryGetInventoryItem(user_id,"cigarette",1) then
+			vRPclient.notify(player,{"Smoking cigarette."})
+			smoke(player)
+			vRP.closeMenu(player)
+		end
+	end
+end} 
+
 items["pills"] = {"Pills","A simple healing medication.",pills_choices,0.1}
+items["cigarette"] = {"Cigarette","A small cylinder of finely cut tobacco leaves rolled in thin paper for smoking.",cig_choices,0.1}
 
 return items
