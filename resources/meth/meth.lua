77abad7fe3a5123551e655ae9a068d2404b592d3
Ozadu <normpad48@gmail.com>
2017-06-18 00:39:38 -0400

    global smoke, fixed adding/removing players

--- resources/meth/meth.lua
+++ resources/meth/meth.lua
@@ -2,14 +2,24 @@ vRP = Proxy.getInterface("vRP")
 vRPserver = Tunnel.getInterface("vRP","meth")
 methServer = Tunnel.getInterface("methServer","methClient")
 
--- meth = {}
--- Tunnel.bindInterface("methClient",meth)
+meth = {}
+Tunnel.bindInterface("methClient",meth)
 
 methLabs = {
 	"camper",
 	"taco",
 	"journey"
 }
+
+local Keys = {
+	["E"] = 38
+}
+
+function DisplayHelpText(str)
+	SetTextComponentFormat("STRING")
+	AddTextComponentString(str)
+	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
 
 --check if a given car is a meth lab
 function isCarMethLab(carModel)
@@ -27,9 +37,10 @@ function getCarName(carModel)
 	return nil
 end
 
-local smokeRunning = false
-function startParticles(car)
-	if smokeRunning then return end
+
+local smokes = {}
+function meth.addSmoke(vehicleId)
+	if smokes[vehicleId] ~= nil then return end
 	if not HasNamedPtfxAssetLoaded("core") then
 		RequestNamedPtfxAsset("core")
 		while not HasNamedPtfxAssetLoaded("core") do
@@ -38,9 +49,17 @@ function startParticles(car)
 	end
 
 	SetPtfxAssetNextCall("core")
-	local part = StartParticleFxLoopedOnEntity("ent_amb_smoke_foundry", car, 0.0, 0.0, 2.0, 0.0,0.0,0.0,1.0, false, false, false)
-	smokeRunning = true
+	local part = StartParticleFxLoopedOnEntity("ent_amb_smoke_foundry", vehicleId, 0.0, 0.0, 2.0, 0.0,0.0,0.0,1.0, false, false, false)
+	smokes[vehicleId] = part
 end
+
+function meth.removeSmoke(vehicleId)
+	RemoveParticleFx(smokes[vehicleId])
+	smoke[vehicleId] = nil
+end
+
+local currentMethLab = nil
+local cookingMeth = false
 
 --Thread
 Citizen.CreateThread(function()
@@ -50,13 +69,48 @@ Citizen.CreateThread(function()
 		local car = GetVehiclePedIsIn(ped, false)
 		
 		if car then
-			local carModel = GetEntityModel(car)
-			local carName = getCarName(carModel)
-			if isCarMethLab(carModel) then 
-				methServer.enterMethLab({car,carModel,carName})
-				startParticles(car)
-				Citizen.Trace("DEBUG: Entering meth Lab")
+			if car ~= 0 and GetEntitySpeed(car) < 1 and not cookingMeth then 
+				local carModel = GetEntityModel(car)
+				if isCarMethLab(carModel) then 
+					startLabOption()
+				end
 			end
 		end
 	end
 end)
+
+--gives player the option to start the meth lab
+function startLabOption()
+	Citizen.CreateThread(function()
+		while true do
+			Citizen.Wait(10)
+			local ped = GetPlayerPed(-1)
+			local car = GetVehiclePedIsIn(ped, false)
+			if car == 0 or GetEntitySpeed(car) > 1 then break end
+			DisplayHelpText("Press ~g~E~s~ to start cooking")
+			if IsControlJustReleased(1, Keys['E']) then 
+				local carModel = GetEntityModel(car)
+				local carName = getCarName(carModel)
+				currentMethLab = car
+				methServer.enterMethLab({car,carModel,carName})
+				startCooking()
+				Citizen.Trace("DEBUG: Entering meth Lab")
+				break				
+			end
+		end
+	end)
+end
+
+function startCooking()
+	Citizen.CreateThread(function()
+		cookingMeth = true
+		while cookingMeth do 
+			Citizen.Wait(10)
+			local ped = GetPlayerPed(-1)
+			local car = GetVehiclePedIsIn(ped, false)
+			if car == 0 or GetEntitySpeed(car) > 1 then cookingMeth = false end
+		end
+		methServer.exitMethLab({currentMethLab})
+		currentMethLab = nil
+	end)
+end
--- resources/meth/methServer.lua
+++ resources/meth/methServer.lua
@@ -4,11 +4,12 @@ local cfg = require("resources/meth/config")
 
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP","methServer")
+methClient = Tunnel.getInterface("methClient","methServer")
 
 meth = {}
 Tunnel.bindInterface("methServer",meth)
 
-local activeMethLabs = {}
+activeMethLabs = {}
 
 --player enters a meth lab, adds meth lab if one doesn't exist, joins existing one if it does
 function meth.enterMethLab(vehicleId,vehicleModel,vehiceName)
@@ -16,9 +17,20 @@ function meth.enterMethLab(vehicleId,vehicleModel,vehiceName)
 		vRP.getUserId({source},function(user_id)
 			addMethLab(vehicleId,vehiceName,user_id)
 		end)
-	else
-		activeMethLabs[vehicleId].players[source] = true
 	end
+	activeMethLabs[vehicleId].players[source] = true
+	print("Adding player to meth lab")
+end
+
+--player leaves a meth lab
+function meth.exitMethLab(vehicleId)
+	if activeMethLabs[vehicleId] ~= nil then
+		activeMethLabs[vehicleId].players[source] = nil
+	end
+	if #(activeMethLabs[vehicleId].players) == 0 then
+		removeMethLab(vehicleId)
+	end
+	print("Removing player from meth lab")
 end
 
 --adds a meth lab
@@ -26,24 +38,31 @@ function addMethLab(vehicleId,name,user_id)
 	local methLab = {}
 	methLab.players = {}
 	methLab.chestname = "u"..user_id.."veh_"..string.lower(name)
+	methLab.running = false
 	
 	--get chest data
 	vRP.getSData({"chest:"..methLab.chestname},function(items)
 		methLab.items = json.decode(items) or {}
 	end)
 	activeMethLabs[vehicleId] = methLab
+	addSmoke(vehicleId)
 	print("Meth lab added.")
 end 
 
 --removes a meth lab
 function removeMethLab(vehicleId)
 	activeMethLabs[vehicleId] = nil
+	methClient.removeSmoke(-1,{vehicleId})
+	print("Meth lab removed")
+end
+
+function addSmoke(vehicleId)
+	methClient.addSmoke(-1,{vehicleId})
 end
 
 --processes a tick of the meth lab, removes reagent and adds products
 function methLabTick(lab)
 	for k,v in pairs(lab.players) do
-	
 		--check if vehicle has meth ingredients
 		local reagents_ok = true
         for reagent,amount in pairs(cfg.methIngredients) do
@@ -58,6 +77,9 @@ function methLabTick(lab)
 		--take ingredients from car
 		for reagent,amount in pairs(cfg.methIngredients) do
 			lab.items[reagent].amount = lab.items[reagent].amount - amount
+			if lab.items[reagent].amount == 0 then 
+				lab.items[reagent] = nil
+			end
         end
 		
 		--add products
@@ -74,12 +96,11 @@ function methLabTick(lab)
 end
 
 function loop()
-	
 	for k,v in pairs(activeMethLabs) do
 		print("Processing tick for " .. v.chestname)
 		methLabTick(v)
 	end
-	SetTimeout(3000,loop)
+	SetTimeout(10000,loop)
 end
 
 loop()
