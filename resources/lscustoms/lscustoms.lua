9639e647493dcbfaf9e434c660a112ffa632350a
Serpico <bamboozol@gmail.com>
2017-06-18 00:32:20 -0400

    Added carshop location as listed in trello. Moved positioning of ls customs in senora to be inside the building

--- resources/lscustoms/lscustoms.lua
+++ resources/lscustoms/lscustoms.lua
@@ -11,7 +11,7 @@ local lsc = {
 		[1] = { locked = false, outside = { x = -362.7962, y = -132.4005, z = 38.25239, heading = 71.187133}, inside = {x = -337.3863,y = -136.9247,z = 38.5737, heading = 269.455}},
 		[2] = { locked = false, outside = { x = -1140.191, y = -1985.478, z = 12.72923, heading = 315.290466}, inside = {x = -1155.536,y = -2007.183,z = 12.744, heading = 155.413}},
 		[3] = { locked = false, outside = { x = 716.4645, y = -1088.869, z = 21.92979, heading = 88.768}, inside = {x = 731.8163,y = -1088.822,z = 21.733, heading = 269.318}},
-		[4] = { locked = false, outside = { x = 1174.811, y = 2649.954, z = 37.37151, heading = 0.450}, inside = {x = 1175.04,y = 2640.216,z = 37.32177, heading = 182.402}},
+		[4] = { locked = false, outside = { x = 1175.04, y = 2640.216, z = 37.32177, heading = 0.450}, inside = {x = 1175.04,y = 2640.216,z = 37.32177, heading = 182.402}},
 		--[5] = { locked = false, outside = { x = 241.811, y = -769.954, z = 30.37151, heading = 0.450}, inside = {x = 241.811, y = -769.954, z = 30.37151, heading = 182.402}},
 		[5] = { locked = false, outside = { x = -211.782, y = -1323.854, z = 30.51124, heading = 0.450}, inside = { x = -211.782, y = -1323.854, z = 30.51124, heading = 0.450}},
 		[6] = { locked = false, outside = { x = 115.734046936035, y = 6621.92724609375, z = 31.8413715362549, heading = 0.450}, inside = { x = 115.734046936035, y = 6621.92724609375, z = 31.8413715362549, heading = 0.450}}
