5e0ef1da5ea1ad3ed266825cad361ce8db918455
Serpico <bamboozol@gmail.com>
2017-06-23 12:45:40 -0400

    If I understand what's being said. Players start getting put in the queue at 28 players and this should allow us 4 to get in quick on server restarts. Probably can be tweaked or set to false down the road.

--- resources/pQueue/server.lua
+++ resources/pQueue/server.lua
@@ -5,7 +5,7 @@ local Config = {}
 
 Config.PlayerLimit = 32 -- PLAYERCAP                -- How many playerslots your server has
 
-Config.PlaceInQueueAt = 15             --[[ This determines when it will start placing players in the queue. For example, if the server has 5 people ingame, and this is set to 5, it will start placing players in queue.
+Config.PlaceInQueueAt = 28             --[[ This determines when it will start placing players in the queue. For example, if the server has 5 people ingame, and this is set to 5, it will start placing players in queue.
                                             If there were 3 people in the server and it was set to 5, it would allow 2 more people to join without going through the queue.
                                             Setting this to false will disable it and will only place players in queue when the server is full. This is useful for server restarts which will assure priority users get in.]]
 
