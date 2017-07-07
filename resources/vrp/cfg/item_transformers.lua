1dcd3b9df7f77750da23624bd1e0ca30b4dcc255
Serpico <bamboozol@gmail.com>
2017-07-07 19:40:37 -0400

    Added body building. Commented out for now as it's too fast a rate

--- resources/vrp/cfg/item_transformers.lua
+++ resources/vrp/cfg/item_transformers.lua
@@ -502,7 +502,27 @@ cfg.item_transformers = {
           		}
 			}
 		}
-	}
+	}--[[,
+	{
+		name="Body training", -- menu name
+		r=255,g=125,b=0, -- color
+		max_units=1000,
+		units_per_minute=1000,
+		x=-1202.96252441406,y=-1566.14086914063,z=4.61040639877319,
+		radius=7.5, height=1.5, -- area
+		recipes = {
+		  	["Strength"] = { -- action name
+			    description="Increase your strength.", -- action description
+			    in_money=0, -- money taken per unit
+			    out_money=0, -- money earned per unit
+			    reagents={}, -- items taken per unit
+			    products={}, -- items given per unit
+			    aptitudes={ -- optional
+		      		["physical.strength"] = 1 -- "group.aptitude", give 1 exp per unit
+		    	}
+		  	}
+		}
+	}]]-- Rate is wayyyyy too faste
 }
 
 -- define transformers randomly placed on the map
