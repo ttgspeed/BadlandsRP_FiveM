9b190c77eca6fc6f11ac2c6d081288f1736fda79
Serpico <bamboozol@gmail.com>
2017-07-08 01:16:28 -0400

    Orginal value. was decreased for instance check. Let me know if any questions

--- resources/vrp/cfg/base.lua
+++ resources/vrp/cfg/base.lua
@@ -13,7 +13,7 @@ cfg.whitelist = false -- enable/disable whitelist
 
 -- delay the tunnel at loading (for weak connections)
 cfg.load_duration = 10 -- seconds, player duration in loading mode at the first spawn
-cfg.load_delay = 5 -- milliseconds, delay the tunnel communication when in loading mode
+cfg.load_delay = 60 -- milliseconds, delay the tunnel communication when in loading mode
 cfg.global_delay = 0 -- milliseconds, delay the tunnel communication when not in loading mode
 
 cfg.ping_timeout = 5 -- number of minutes after a client should be kicked if not sending pings
