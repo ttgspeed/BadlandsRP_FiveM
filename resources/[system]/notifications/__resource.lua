7555c4d89788fbf2b56cd71744b12f27f773dedf
ttgspeed <ttg.speed@gmail.com>
2017-07-14 20:31:04 -0400

    Server restart notifications

---
+++ resources/[system]/notifications/__resource.lua
@@ -0,0 +1,21 @@
+description 'Server Notifications'
+
+server_script 'server.lua'
+client_script {
+	'Tunnel.lua',
+	'client.lua',
+}
+
+ui_page 'restart.html'
+
+files {
+	'restart.html',
+	'css/bootstrap.css',
+	'css/bootstrap.min.css',
+	'css/homepage.css',
+	'js/bootstrap.js',
+	'js/bootstrap.min.js',
+	'js/jquery.js',
+	'logo.jpg',
+	'cursor.webp'
+}
