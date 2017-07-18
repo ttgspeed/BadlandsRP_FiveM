1c37ec77165b0f06eae81470230b62476dbac967
Serpico <bamboozol@gmail.com>
2017-07-17 20:22:55 -0400

    No longer need to press enter when revived by a medic. Players should no longer bleed out when revived by a medic.

--- resources/vrp/cfg/client.lua
+++ resources/vrp/cfg/client.lua
@@ -42,7 +42,7 @@ cfg.handcuff_disable_menu = true
 cfg.coma_threshold = 105
 
 -- maximum duration of the coma in minutes
-cfg.coma_duration = 5
+cfg.coma_duration = 3
 
 -- if true, a player in coma will not be able to open the main menu
 cfg.coma_disable_menu = true
