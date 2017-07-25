17a1bc143e03826ab674e77eb16a7c24d7ef3e9b
Serpico <bamboozol@gmail.com>
2017-07-24 21:14:06 -0400

    "spawn protect" players while disclaimer is up

--- resources/disclaimer/client.lua
+++ resources/disclaimer/client.lua
@@ -12,15 +12,18 @@ AddEventHandler('onClientMapStart', function()
       if (GetTimeDifference(GetGameTimer(), startTime) > delay) then
         display = false
         TriggerEvent('disclaimer:display', false)
+        tvRP.setFriendlyFire(false)
       end
       if (GetTimeDifference(GetGameTimer(), startTime) > keepOpenTime) then
         ShowInfo('~y~Read carefully.~w~ Press ~INPUT_CONTEXT~ to close.', 0)
         if (IsControlJustPressed(1, 51)) then
           display = false
           TriggerEvent('disclaimer:display', false)
+          tvRP.setFriendlyFire(false)
         end
       else
         ShowInfo('~y~Read carefully.~w~ This window can be closed after 45 seconds.', 0)
+        tvRP.setFriendlyFire(true)
       end
     end
   end)
