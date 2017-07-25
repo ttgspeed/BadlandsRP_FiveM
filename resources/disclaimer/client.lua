cde49d404cd1fde106c615c07b660e55c95caa28
Serpico <bamboozol@gmail.com>
2017-07-24 20:45:03 -0400

    Keep disclaimer open for 45 seconds before player can close it.

--- resources/disclaimer/client.lua
+++ resources/disclaimer/client.lua
@@ -3,19 +3,24 @@ AddEventHandler('onClientMapStart', function()
     local display = true
     local startTime = GetGameTimer()
     local delay = 60000 -- ms
+    local keepOpenTime = 45000
 
     TriggerEvent('disclaimer:display', true)
 
     while display do
       Citizen.Wait(1)
-      ShowInfo('~y~Read carefully.~w~ Press ~INPUT_CONTEXT~ to close.', 0)
       if (GetTimeDifference(GetGameTimer(), startTime) > delay) then
         display = false
         TriggerEvent('disclaimer:display', false)
       end
-      if (IsControlJustPressed(1, 51)) then
-        display = false
-        TriggerEvent('disclaimer:display', false)
+      if (GetTimeDifference(GetGameTimer(), startTime) > keepOpenTime) then
+        ShowInfo('~y~Read carefully.~w~ Press ~INPUT_CONTEXT~ to close.', 0)
+        if (IsControlJustPressed(1, 51)) then
+          display = false
+          TriggerEvent('disclaimer:display', false)
+        end
+      else
+        ShowInfo('~y~Read carefully.~w~ This window can be closed after 45 seconds.', 0)
       end
     end
   end)
