f3f03b480c30ec22bdcfa999e32081a7311dd4b4
ttgspeed <ttg.speed@gmail.com>
2017-07-30 20:58:28 -0400

    Exterior resource fixes

--- resources/bank_heist/server.lua
+++ resources/bank_heist/server.lua
@@ -1,7 +1,5 @@
---local Proxy = load(LoadResourceFile("vrp", "lib/Proxy"), "lib/Proxy")()
---local Tunnel = load(LoadResourceFile("vrp", "lib/Tunnel"), "lib/Tunnel")()
-local Proxy = load(LoadResourceFile("vrp", "lib/Proxy"), "lib/Proxy")()
-local Tunnel = load(LoadResourceFile("vrp", "lib/Tunnel"), "lib/Tunnel")()
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP","bankHeist")
 
