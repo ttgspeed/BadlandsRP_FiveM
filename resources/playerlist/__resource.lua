40045cff121932354faddf40e7357ebb2578a110
ttgspeed <ttg.speed@gmail.com>
2017-08-01 21:43:37 -0400

    FX Full Structure

---
+++ resources/playerlist/__resource.lua
@@ -0,0 +1,8 @@
+client_script 'playerlist.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+}
---
+++ resources/playerlist/playerlist.lua
@@ -0,0 +1,38 @@
+--Playerlist created by Arturs
+local plist = false
+function ShowPlayerList()
+	if plist == false then
+		local players 
+		players = '<tr class= "titles"><th class="name">Name</th><th class="id">ID</th></tr>'
+		for i = 0,32 do
+			if NetworkIsPlayerActive(GetPlayerFromServerId(i)) then --Check if player is active
+				--if i ~= PlayerId() then -- enable this check to not display local player
+					players = players..' <tr class="player"><th class="name">'..GetPlayerName(GetPlayerFromServerId(i))..'</th>'..' <th class="id">'..i..'</th></tr>'
+				--end
+			end
+		end
+		players = players
+		SendNUIMessage({
+			meta = 'open',
+			players = players
+		})
+		plist = true
+	else
+		SendNUIMessage({ 
+			meta = 'close'
+		})
+		plist = false
+	end
+end
+
+Citizen.CreateThread( function()
+	while true do
+		Citizen.Wait(0)
+		--Displays playerlist when player hold X
+		if IsControlJustPressed(1, 167) then --Start holding
+			ShowPlayerList()
+		elseif IsControlJustReleased(1, 167) then --Stop holding
+			ShowPlayerList()
+		end
+	end
+end)
