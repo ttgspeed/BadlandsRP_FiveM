891b6cc7f442153e127f7925a118ecaeb86b0c04
Serpico <bamboozol@gmail.com>
2017-08-07 23:01:49 -0400

    fix use meth

--- resources/vrp/cfg/item/drugs.lua
+++ resources/vrp/cfg/item/drugs.lua
@@ -173,7 +173,7 @@ end}
 items["pills"] = {"Pills","A simple healing medication.",function(args) return pills_choices end,0.1}
 items["cigarette"] = {"Cigarette","A small cylinder of finely cut tobacco leaves rolled in thin paper for smoking.",function(args) return cig_choices end,0.1}
 items["weed"] = {"Weed", "It's 'medicinal'",function(args) return weed_choices end, 0.5}
-items["meth"] = {"Meth", "", meth_choices, 0.5}
+items["meth"] = {"Meth", "",function(args) return meth_choices end, 0.5}
 items["meth_kit"] = {"Mobile Meth Lab Kit", "Converts your vehicle into a mobile meth lab. Must be used on a large camper type vehicle.",function(args) return meth_kit_choices end,5.0}
 
 return items
