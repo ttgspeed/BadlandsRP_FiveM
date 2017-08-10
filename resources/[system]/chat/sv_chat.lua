411eceba98d1acdeb21bb61bc42c00237078504a
Serpico <bamboozol@gmail.com>
2017-08-10 01:10:55 -0400

    ooc/tweet/proximity chat. Name and ID not in yet.

--- resources/[system]/chat/sv_chat.lua
+++ resources/[system]/chat/sv_chat.lua
@@ -27,7 +27,7 @@ AddEventHandler('__cfx_internal:commandFallback', function(command)
     TriggerEvent('chatMessage', source, name, '/' .. command)
 
     if not WasEventCanceled() then
-        TriggerClientEvent('chatMessage', -1, name, { 255, 255, 255 }, '/' .. command) 
+        TriggerClientEvent('chatMessage', -1, name, { 255, 255, 255 }, '/' .. command)
     end
 
     CancelEvent()
@@ -44,4 +44,25 @@ end)
 
 RegisterCommand('say', function(source, args, rawCommand)
     TriggerClientEvent('chatMessage', -1, (source == 0) and 'console' or GetPlayerName(source), { 255, 255, 255 }, rawCommand:sub(5))
+end)
+
+AddEventHandler('chatMessage', function(source, name, message)
+    if string.sub(message,1,string.len("/"))=="/" then
+        local commandEnd = string.find(message,"%s")
+        local msg = string.sub(message,commandEnd+1)
+        local cmd = string.sub(message,2,commandEnd-1)
+        cmd = string.lower(cmd)
+        if cmd == "ooc" or cmd == "g" then
+            TriggerClientEvent('chatMessage', -1, "^1[^0OOC", {100, 100, 100}, "^4 " .. name.. " ^4(^0".."".."^4): ^0" .. msg .. "^1]")
+        elseif cmd == "tweet" then
+            TriggerClientEvent('chatMessage', -1, "^5Twitter", {100, 100, 100}, "^4 @" ..name.. " ^4(^0".."".."^4): ^0" .. msg)
+        elseif cmd == "help" or cmd == "h" then
+            TriggerClientEvent('sendPlayerMesage',-1, source, name.."("..""..")", "^1Common controls: ^0M = Open menu ^1|| ^0X = Toggle hands up/down ^1|| ^0~ = Toggle your voice volume ^1|| ^0L = Toggle car door locks ^1|| ^0/ooc = For out of character chat")
+        else
+            TriggerClientEvent('sendPlayerMesage',-1, source, name.."("..""..")", "Invalid command")
+        end
+    else
+        TriggerClientEvent("sendProximityMessage", -1, source, name.."("..""..")", message)
+    end
+    CancelEvent()
 end)
