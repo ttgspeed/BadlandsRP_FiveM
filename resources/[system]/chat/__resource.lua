b755df43913474063e0da64c9e0997e0c2a14f81
Serpico <bamboozol@gmail.com>
2017-08-11 01:23:20 -0400

    added vRP ID in chat

--- resources/[system]/chat/__resource.lua
+++ resources/[system]/chat/__resource.lua
@@ -3,6 +3,7 @@ description 'chat management stuff'
 ui_page 'html/index.html'
 
 client_script 'cl_chat.lua'
+server_script "@vrp/lib/utils.lua"
 server_script 'sv_chat.lua'
 
 files {
--- resources/[system]/chat/sv_chat.lua
+++ resources/[system]/chat/sv_chat.lua
@@ -1,3 +1,8 @@
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+
+vRP = Proxy.getInterface("vRP")
+
 RegisterServerEvent('chat:init')
 RegisterServerEvent('chat:addTemplate')
 RegisterServerEvent('chat:addMessage')
@@ -12,13 +17,18 @@ AddEventHandler('_chat:messageEntered', function(author, color, message)
         return
     end
 
-    TriggerEvent('chatMessage', source, author, message)
+    local user_id = vRP.getUserId({source})
+    if user_id ~= nil then
+        TriggerEvent('chatMessage', source, author, message, user_id)
 
-    if not WasEventCanceled() then
-        TriggerClientEvent('chatMessage', -1, author,  { 255, 255, 255 }, message)
+        if not WasEventCanceled() then
+            TriggerClientEvent('chatMessage', -1, author,  { 255, 255, 255 }, message)
+        end
+
+        print("-- CHAT LOG -- ("..user_id..") "..author .. ': ' .. message)
+    else
+        CancelEvent()
     end
-
-    print(author .. ': ' .. message)
 end)
 
 AddEventHandler('__cfx_internal:commandFallback', function(command)
@@ -46,23 +56,23 @@ RegisterCommand('say', function(source, args, rawCommand)
     TriggerClientEvent('chatMessage', -1, (source == 0) and 'console' or GetPlayerName(source), { 255, 255, 255 }, rawCommand:sub(5))
 end)
 
-AddEventHandler('chatMessage', function(source, name, message)
+AddEventHandler('chatMessage', function(source, name, message, user_id)
     if string.sub(message,1,string.len("/"))=="/" then
         local commandEnd = string.find(message,"%s")
         local msg = string.sub(message,commandEnd+1)
         local cmd = string.sub(message,2,commandEnd-1)
         cmd = string.lower(cmd)
         if cmd == "ooc" or cmd == "g" then
-            TriggerClientEvent('chatMessage', -1, "^1[^0OOC", {100, 100, 100}, "^4 " .. name.. " ^4(^0".."".."^4): ^0" .. msg .. "^1]")
+            TriggerClientEvent('chatMessage', -1, "^1[^0OOC", {100, 100, 100}, "^4 " .. name.. " ^4(^0"..user_id.."^4): ^0" .. msg .. "^1]")
         elseif cmd == "tweet" then
-            TriggerClientEvent('chatMessage', -1, "^5Twitter", {100, 100, 100}, "^4 @" ..name.. " ^4(^0".."".."^4): ^0" .. msg)
+            TriggerClientEvent('chatMessage', -1, "^5Twitter", {100, 100, 100}, "^4 @" ..name.. " ^4(^0"..user_id.."^4): ^0" .. msg)
         elseif cmd == "help" or cmd == "h" then
-            TriggerClientEvent('sendPlayerMesage',-1, source, name.."("..""..")", "^1Common controls: ^0M = Open menu ^1|| ^0X = Toggle hands up/down ^1|| ^0~ = Toggle your voice volume ^1|| ^0L = Toggle car door locks ^1|| ^0/ooc = For out of character chat")
+            TriggerClientEvent('sendPlayerMesage',-1, source, name.."("..user_id..")", "^1Common controls: ^0M = Open menu ^1|| ^0X = Toggle hands up/down ^1|| ^0~ = Toggle your voice volume ^1|| ^0L = Toggle car door locks ^1|| ^0/ooc = For out of character chat")
         else
-            TriggerClientEvent('sendPlayerMesage',-1, source, name.."("..""..")", "Invalid command")
+            TriggerClientEvent('sendPlayerMesage',-1, source, name.."("..user_id..")", "Invalid command")
         end
     else
-        TriggerClientEvent("sendProximityMessage", -1, source, name.."("..""..")", message)
+        TriggerClientEvent("sendProximityMessage", -1, source, name.."("..user_id..")", message)
     end
     CancelEvent()
 end)
