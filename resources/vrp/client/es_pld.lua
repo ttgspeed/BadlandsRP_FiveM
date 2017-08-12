e14a15bbceef753d0a25c5cdb1b00635ded93337
Serpico <bamboozol@gmail.com>
2017-08-12 17:12:51 -0400

    nil check in pld

--- resources/vrp/client/es_pld.lua
+++ resources/vrp/client/es_pld.lua
@@ -71,10 +71,14 @@ Citizen.CreateThread(function()
 						SetTextEdge(0, 0, 0, 0, 0);
 						SetTextEntry("STRING");
 						SetTextCentre(1)
+						local user_id = tvRP.getUserId(GetPlayerServerId(i))
+						if not user_id then
+							user_id = "unk"
+						end
 						if NetworkIsPlayerTalking(i) then
-							AddTextComponentString("~b~"..tvRP.getUserId(GetPlayerServerId(i)))
+							AddTextComponentString("~b~"..user_id)
 						else
-							AddTextComponentString(tvRP.getUserId(GetPlayerServerId(i)))
+							AddTextComponentString(user_id)
 						end
 						DrawText(y, z)
 --[[
@@ -118,15 +122,18 @@ Citizen.CreateThread(function()
 						if(t == 1)then
 								drawTxt(0.515, 0.95, 1.0,1.0,0.4, "~y~Talking", 255, 255, 255, 255)
 						end
-
+						local user_id = tvRP.getUserId(GetPlayerServerId(i))
+						if not user_id then
+							user_id = "unk"
+						end
 						if GetPlayerPed(i) == GetPlayerPed(-1) then
 							if tvRP.isWhispering() then
-								drawTxt(0.520, 0.95 + (t * 0.023), 1.0,1.0,0.4, "~b~You: ~w~"..tvRP.getUserId(GetPlayerServerId(i)).." ~b~(Whispering)", 255, 255, 255, 255)
+								drawTxt(0.520, 0.95 + (t * 0.023), 1.0,1.0,0.4, "~b~You: ~w~"..user_id.." ~b~(Whispering)", 255, 255, 255, 255)
 							else
-								drawTxt(0.520, 0.95 + (t * 0.023), 1.0,1.0,0.4, "~b~You: ~w~"..tvRP.getUserId(GetPlayerServerId(i)), 255, 255, 255, 255)
+								drawTxt(0.520, 0.95 + (t * 0.023), 1.0,1.0,0.4, "~b~You: ~w~"..user_id, 255, 255, 255, 255)
 							end
 						else
-							drawTxt(0.520, 0.95 + (t * 0.023), 1.0,1.0,0.4, ""..tvRP.getUserId(GetPlayerServerId(i)), 255, 255, 255, 255)
+							drawTxt(0.520, 0.95 + (t * 0.023), 1.0,1.0,0.4, ""..user_id, 255, 255, 255, 255)
 						end
 					end
 				end
