25cfcfbcfbab0d035328744c65b85ef96181c1a5
Serpico <bamboozol@gmail.com>
2017-08-14 21:46:32 -0400

    pull from vRP. hide vRP menu when pause menu open.

--- resources/vrp/gui/main.js
+++ resources/vrp/gui/main.js
@@ -22,7 +22,7 @@ window.addEventListener("load",function(){
   dynamic_menu.onValid = function(choice,mod){ $.post("http://vrp/menu",JSON.stringify({act: "valid", id: dynamic_menu.id, choice: choice, mod: mod})); }
 
  //request config
- $.post("http://vrp/cfg",""); 
+ $.post("http://vrp/cfg","");
 
   var current_menu = dynamic_menu;
   var pbars = {}
@@ -42,6 +42,12 @@ window.addEventListener("load",function(){
 
     if(data.act == "cfg"){
       cfg = data.cfg
+    }
+    else if(data.act == "pause_change"){
+      if(data.paused)
+        $(document.body).hide();
+      else
+        $(document.body).show();
     }
     else if(data.act == "open_menu"){ //OPEN DYNAMIC MENU
       current_menu.close();
@@ -86,7 +92,7 @@ window.addEventListener("load",function(){
         delete pbars[data.name];
       }
     }
-    // PROMPT 
+    // PROMPT
     else if(data.act == "prompt"){
       wprompt.open(data.title,data.text);
     }
