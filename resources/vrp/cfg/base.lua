ef38606a98d16c545a8e7a0dab7bc50911a1e28e
Serpico <bamboozol@gmail.com>
2017-08-14 21:39:14 -0400

    Same ip fix

--- resources/vrp/cfg/base.lua
+++ resources/vrp/cfg/base.lua
@@ -11,6 +11,8 @@ cfg.db = {
 cfg.save_interval = 60 -- seconds
 cfg.whitelist = true -- enable/disable whitelist
 
+cfg.ignore_ip_identifier = true
+
 -- delay the tunnel at loading (for weak connections)
 cfg.load_duration = 10 -- seconds, player duration in loading mode at the first spawn
 cfg.load_delay = 30 -- milliseconds, delay the tunnel communication when in loading mode
