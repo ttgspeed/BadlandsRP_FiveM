5b79576209895e6e8cf2715fe0ed602a5f4a5d86
Serpico <bamboozol@gmail.com>
2017-08-26 01:29:23 -0400

    Added prevention of combat while the 1 minute jumbotron is shown. Common complaint from players.

--- resources/[system]/notifications/client.lua
+++ resources/[system]/notifications/client.lua
@@ -1,5 +1,27 @@
+local restart_notice_displayed = false
+
 RegisterNetEvent('notifications:1mWarning')
 AddEventHandler('notifications:1mWarning', function()
+  restart_notice_displayed = true
+  Citizen.CreateThread(function()
+    while restart_notice_displayed do
+      Citizen.Wait(1)
+      DisableControlAction(0,24,true) -- disable attack
+      DisableControlAction(0,25,true) -- disable aim
+      DisableControlAction(0,47,true) -- disable weapon
+      DisableControlAction(0,58,true) -- disable weapon
+      DisableControlAction(0,263,true) -- disable melee
+      DisableControlAction(0,264,true) -- disable melee
+      DisableControlAction(0,257,true) -- disable melee
+      DisableControlAction(0,140,true) -- disable melee
+      DisableControlAction(0,141,true) -- disable melee
+      DisableControlAction(0,142,true) -- disable melee
+      DisableControlAction(0,143,true) -- disable melee
+      DisableControlAction(0,47,true) -- disable weapon
+      DisableControlAction(0,58,true) -- disable weapon
+      DisablePlayerFiring(GetPlayerPed(-1), true) -- Disable weapon firing
+    end
+  end)
   SetNuiFocus(true)
   SendNUIMessage({
       type = "enableui",
@@ -9,6 +31,7 @@ AddEventHandler('notifications:1mWarning', function()
 end)
 
 RegisterNUICallback('escape', function(data, cb)
+    restart_notice_displayed = false
     print("escape")
     SetNuiFocus(false)
     SendNUIMessage({
