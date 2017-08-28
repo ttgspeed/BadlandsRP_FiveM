5784e51f54afda490aab59be6e16a96b860f04b5
Serpico <bamboozol@gmail.com>
2017-08-27 21:50:59 -0400

    unused file ref

--- resources/addons/__resource.lua
+++ resources/addons/__resource.lua
@@ -18,5 +18,3 @@ data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'HANDLING_FILE' 'vehicleaihandlinginfo.meta'
 
--- and a client script
-client_script 'vehicle_names.lua'
