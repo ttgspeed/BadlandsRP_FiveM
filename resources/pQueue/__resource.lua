196a4d4c78d8ff8cb1375b34867f85e0bbb070b5
Serpico <bamboozol@gmail.com>
2017-09-13 16:48:14 -0400

    New player queue to test with build 370

--- resources/pQueue/__resource.lua
+++ resources/pQueue/__resource.lua
@@ -1,5 +1,7 @@
-description "An advanced queue system."
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 client_script "client.lua"
-
 server_script "server.lua"
+
+server_export "AddPriority"
+server_export "RemovePriority"
