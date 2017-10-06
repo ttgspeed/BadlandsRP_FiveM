16555ed11b4dfe1289cf8991fa23589dd65644f9
ttgspeed <ttg.speed@gmail.com>
2017-10-06 17:18:40 -0400

    Trucking job almost done

--- resources/TruckerJob/server.lua
+++ resources/TruckerJob/server.lua
@@ -1,13 +1,13 @@
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
- 
+
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP","vRP_truckerJob")
 
 RegisterServerEvent('truckerJob:success') -- calls the event from client file
 AddEventHandler('truckerJob:success', function(amount) -- handles the event
     local user_id = vRP.getUserId({source})
-	local player = vRP.getUserSource({user_id})
+	  local player = vRP.getUserSource({user_id})
     vRP.giveMoney({user_id,amount})
-    vRPclient.notify(player,{"You've been paid ~g~"..amount.."."})
+    vRPclient.notify(player,{"You've received $"..amount.." for completing your delivery."})
 end)
