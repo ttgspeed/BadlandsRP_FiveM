1a5920b47e02a6a03845c4ca0b874d156d8df4ad
ttgspeed <ttg.speed@gmail.com>
2017-10-06 13:22:41 -0400

    Somehow lost these from last commit

---
+++ resources/CustomScripts/__resource.lua
@@ -0,0 +1,11 @@
+-- server scripts
+server_scripts{ 
+  "deletepoliceweapons-server.lua"
+}
+
+-- client scripts
+client_scripts{
+  "crouch-client.lua",
+  "taxi-client.lua",
+  "missiontext-client.lua"
+ }
