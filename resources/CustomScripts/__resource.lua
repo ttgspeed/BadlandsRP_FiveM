ba5409b6ab092b8afe0894bb0814515576631b12
Serpico <bamboozol@gmail.com>
2017-10-10 16:42:57 -0400

    Moved crouch ability into vRP. Removed call for non existant server script

--- resources/CustomScripts/__resource.lua
+++ resources/CustomScripts/__resource.lua
@@ -1,11 +1,5 @@
--- server scripts
-server_scripts{ 
-  "deletepoliceweapons-server.lua"
-}
-
 -- client scripts
 client_scripts{
-  "crouch-client.lua",
   "taxi-client.lua",
   "missiontext-client.lua"
  }
--- resources/vrp/client/player_state.lua
+++ resources/vrp/client/player_state.lua
@@ -366,6 +366,40 @@ Citizen.CreateThread(function()
 end)
 -- END PLAYER POINTING ACTION
 
+-- Player crouch
+local crouched = false
+
+Citizen.CreateThread( function()
+    while true do
+        Citizen.Wait(1)
+
+        local ped = GetPlayerPed(-1)
+
+        if (DoesEntityExist(ped) and not IsEntityDead(ped)) then
+            DisableControlAction(0, 36, true) -- INPUT_DUCK
+
+            if (not IsPauseMenuActive()) then
+                if (IsDisabledControlJustPressed(0, 36)) then
+                    RequestAnimSet("move_ped_crouched")
+
+                    while (not HasAnimSetLoaded("move_ped_crouched")) do
+                        Citizen.Wait(100)
+                    end
+
+                    if (crouched == true) then
+                        ResetPedMovementClipset(ped, 0)
+                        crouched = false
+                    elseif (crouched == false) then
+                        SetPedMovementClipset(ped, "move_ped_crouched", 0.25)
+                        crouched = true
+                    end
+                end
+            end
+        end
+    end
+end)
+-- end player crouch
+
 -- DISABLE SHOOTING FROM VEHICLE START
 -- Author: Scammer
 -- Source: https://forum.fivem.net/t/release-scammers-script-collection-09-03-17/3313
