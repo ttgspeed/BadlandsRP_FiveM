b13846258f3ac9e899924be391d62937e7a06637
Serpico <bamboozol@gmail.com>
2017-10-11 00:53:38 -0400

    Combined all male model into one menu. Same for female. Moved mp models one menu up.

--- resources/vrp/model-menu/client.lua
+++ resources/vrp/model-menu/client.lua
@@ -272,9 +272,10 @@ function Main()
     --Menu.addButton("Customisation", "checkCustomisation", nil)
     Menu.addButton("Male models", "MaleMenu", nil)
     Menu.addButton("Female Models", "FemaleMenu", nil)
+    Menu.addButton("Multiplayer","MPMenu",nil)
     --Menu.addButton("EMS Models","EMSMenu",nil)
     --Menu.addButton("Animals", "AnimalMenu", nil)
-    Menu.addButton("Others", "OtherMenu", nil)
+    --Menu.addButton("Others", "OtherMenu", nil)
 end
 
 function GCustomisation()
@@ -384,7 +385,7 @@ function MPMenu()
     DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
     options.menu_subtitle = "MP Models"
     ClearMenu()
-    Menu.previous = "OtherMenu"
+    Menu.previous = "Main"
     Menu.addButton(freemode_models[Menu.buttonCount+1], "savempmodel", freemode_models[Menu.buttonCount+1])
     Menu.addButton(freemode_models[Menu.buttonCount+1], "savempmodel", freemode_models[Menu.buttonCount+1])
     --Menu.addButton("More", "MPMenu2", nil)
