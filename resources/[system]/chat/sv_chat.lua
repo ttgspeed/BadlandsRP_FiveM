d91ce0b71c22e7316cb0bc46cf2354738590f1ee
ttgspeed <ttg.speed@gmail.com>
2017-10-20 08:39:53 -0400

    /me command

--- resources/[system]/chat/sv_chat.lua
+++ resources/[system]/chat/sv_chat.lua
@@ -75,6 +75,9 @@ AddEventHandler('chatMessage', function(source, name, message, rp_name, user_id)
         elseif cmd == "/tweet" and (msg ~= nil and msg ~= "") then
             TriggerClientEvent('chatMessage', -1, "^5Twitter", {100, 100, 100}, "^4 @" ..rp_name.. " ^4(^0"..user_id.."^4): ^0" .. msg)
             sendToDiscord(name.." ("..rp_name.." - "..user_id..")", "**TWEET**: "..msg)
+        elseif cmd == "/me" and (msg ~= nil and msg ~= "") then
+            TriggerClientEvent("sendMeMessage", -1, source, "^="..rp_name.." ("..user_id..")", "^9 ^="..msg)
+            sendToDiscord(name.." ("..rp_name.." - "..user_id..")", "**ME**: "..msg)
         elseif cmd == "/help" or cmd == "/h" then
             TriggerClientEvent('sendPlayerMesage',source, source, rp_name.."("..user_id..")", "^1Common controls: ^0M = Open menu ^1|| ^0X = Toggle hands up/down ^1|| ^0~ = Toggle your voice volume ^1|| ^0U = Toggle car door locks ^1|| ^0/ooc = For out of character chat")
         else
