4e635f074b9fe337d730d532eb403d968a916869
Serpico <bamboozol@gmail.com>
2017-10-22 14:41:39 -0400

    Anti Cheat report moved to logwrite

--- resources/vrp/anticheat/sv_anticheat.lua
+++ resources/vrp/anticheat/sv_anticheat.lua
@@ -1,10 +1,12 @@
+local Log = module("lib/Log")
+
 RegisterServerEvent("anticheat:kick")
 AddEventHandler("anticheat:kick", function(reason)
 	local user_id = vRP.getUserId(source)
 	if user_id == nil then
 		user_id = "Not loaded in vRP"
 	end
-	print("-- ANTI-CHEAT LOG -- ("..user_id.." - "..GetPlayerEP(source).." - "..vRP.getSourceIdKey(source)..") "..GetPlayerName(source).." -- "..reason)
+	Log.write(user_id,"-- ANTI-CHEAT LOG -- ("..user_id.." - "..GetPlayerEP(source).." - "..vRP.getSourceIdKey(source)..") "..GetPlayerName(source).." -- "..reason, Log.log_type.anticheat)
 	--DropPlayer(source, reason)
 end)
 
@@ -15,7 +17,7 @@ AddEventHandler("anticheat:ban", function(reason, details)
 	if user_id == nil then
 		user_id = "Not loaded in vRP"
 	end
-	print("-- ANTI-CHEAT LOG -- ("..user_id.." - "..GetPlayerEP(source).." - "..vRP.getSourceIdKey(source)..") "..GetPlayerName(source).." -- "..details)
+	Log.write(user_id,"-- ANTI-CHEAT LOG -- ("..user_id.." - "..GetPlayerEP(source).." - "..vRP.getSourceIdKey(source)..") "..GetPlayerName(source).." -- "..details, Log.log_type.anticheat)
 	--vRP.ban(source, reason, 0)
 	-- ban above does a drop, but incase not load in vrp kept, drop here too
 	--DropPlayer(source, reason)
