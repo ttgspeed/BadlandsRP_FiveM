f50eb6c9a19ac94f95f4c5a708cb19471246b864
ttgspeed <ttg.speed@gmail.com>
2017-10-22 20:32:11 -0400

    Fix truckerjob payment not being rounded

--- resources/TruckerJob/truckerjob.lua
+++ resources/TruckerJob/truckerjob.lua
@@ -257,7 +257,7 @@ function tick()
 		local trailerCoords = GetEntityCoords(MISSION.trailer, 0)
 		if ( GetDistanceBetweenCoords(currentMission[1], currentMission[2], currentMission[3], trailerCoords ) < 25 and  not IsEntityAttached(MISSION.trailer)) then
 			MISSION.removeMarker()
-			TriggerServerEvent('truckerJob:success',(currentMission[4]*0.75))
+			TriggerServerEvent('truckerJob:success',(currentMission[4]))
 			clear()
 		elseif ( GetDistanceBetweenCoords(currentMission[1], currentMission[2], currentMission[3], trailerCoords ) < 100 and IsEntityAttached(MISSION.trailer) ) then
 			DrawMarker(1, currentMission[1], currentMission[2], currentMission[3] - 1, 0, 0, 0, 0, 0, 0, 3.0001, 3.0001, 1.5001, 255, 165, 0,165, 0, 0, 0,0)
@@ -299,7 +299,7 @@ function GUI.mission()
 	BLIP.trailer.i = BLIP.trailer.i + 1
 	BLIP.destination.i = BLIP.destination.i + 1
 	currentMission = delivery_locations[randomMission]
-	currentMission[4] = math.ceil(Vdist(pos.x, pos.y, pos.z, currentMission[1], currentMission[2], currentMission[3]))
+	currentMission[4] = math.ceil(Vdist(pos.x, pos.y, pos.z, currentMission[1], currentMission[2], currentMission[3])*0.75)
 	print(currentMission[4])
 	GUI.showMenu = false
 	--mission start
