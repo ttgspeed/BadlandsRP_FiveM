1673883c1721407ec365a460c2151a3442adf066
Serpico <bamboozol@gmail.com>
2017-10-23 20:46:54 -0400

    Removed useless random spawn for frieght. Added second freight trains so 2 of them are running. 1 in each track

--- resources/trains/client.lua
+++ resources/trains/client.lua
@@ -70,37 +70,30 @@ end
 
 LoadTrainModels()
 
-
+-- Track 1
 TrainLocations = {
-{2533.0,2833.0,38.0},
-{2606.0,2927.0,40.0},
-{2463.0,3872.0,38.8},
-{1164.0,6433.0,32.0},
-{537.0,-1324.1,29.1},
-{219.1,-2487.7,6.0}
+	2533.4072265625,2832.7502441406,38.570995330811
 }
 
+-- Track 2
+TrainLocations2 = {
+	2538.73046875,2828.4760742188,38.366275787354
+}
 
 RegisterNetEvent("StartTrain")
 function StartTrain()
 	Citizen.Trace("a train has arrived") -- whee i must be host, lucky me
-		randomSpawn = math.random(#TrainLocations)
-		x,y,z = TrainLocations[randomSpawn][1], TrainLocations[randomSpawn][2], TrainLocations[randomSpawn][3] -- get some random locations for our spawn
-
-	yesorno = math.random(0,100)
-	if yesorno >= 50 then -- untested, but seems to work /shrug
-		yesorno = true
-	elseif yesorno < 50 then
-		yesorno = false
-	end
 	DeleteAllTrains()
-	Train = CreateMissionTrain(math.random(0,22), x,y,z,yesorno)
+	Train = CreateMissionTrain(math.random(0,22), TrainLocations[1], TrainLocations[2], TrainLocations[3],true)
+	Train2 = CreateMissionTrain(math.random(0,22), TrainLocations2[1], TrainLocations2[2], TrainLocations2[3],true)
 	MetroTrain = CreateMissionTrain(24,40.2,-1201.3,31.0,true) -- these ones have pre-defined spawns since they are a pain to set up
 	MetroTrain2 = CreateMissionTrain(24,-618.0,-1476.8,16.2,true)
 	CreatePedInsideVehicle(Train, 26, GetHashKey("s_m_m_lsmetro_01"), -1, 1, true)
+	CreatePedInsideVehicle(Train2, 26, GetHashKey("s_m_m_lsmetro_01"), -1, 1, true)
 	CreatePedInsideVehicle(MetroTrain, 26, GetHashKey("s_m_m_lsmetro_01"), -1, 1, true)
 	CreatePedInsideVehicle(MetroTrain2, 26, GetHashKey("s_m_m_lsmetro_01"), -1, 1, true) -- create peds for the trains
 	SetEntityAsMissionEntity(Train,true,true) -- dunno if this does anything, just throwing it in for good measure
+	SetEntityAsMissionEntity(Train2,true,true) -- dunno if this does anything, just throwing it in for good measure
 	SetEntityAsMissionEntity(MetroTrain,true,true)
 	SetEntityAsMissionEntity(MetroTrain2,true,true)
 
