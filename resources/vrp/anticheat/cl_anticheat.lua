a6f3d9bec64e6fd2d116aa88f73b873e3e0dcf52
Serpico <bamboozol@gmail.com>
2017-10-30 01:41:44 -0400

    Added health regen/force health detection. This would be for a script that ran setting health at 200 in a loop instead of godmode. Added infinite ammo prevention. Moved admin check to the outer most check for it only runs anti cheat on non admins.

--- resources/vrp/anticheat/cl_anticheat.lua
+++ resources/vrp/anticheat/cl_anticheat.lua
@@ -5,7 +5,7 @@ local timeToReset = 0
 local maxTrigger = 3
 
 function tvRP.startCheatCheck()
-	if not threadStarted then
+	if not threadStarted and not tvRP.isAdmin() then
 		threadStarted = true
 		Citizen.CreateThread(function()
 			Citizen.Wait(30000)
@@ -13,7 +13,6 @@ function tvRP.startCheatCheck()
 				Citizen.Wait(1)
 				local playerPed = GetPlayerPed(-1)
 				local playerid = PlayerId()
-				local isAdmin = tvRP.isAdmin()
 
 				-- God mode check, health above limits
 				if GetEntityHealth(playerPed) > 200 or GetPedArmour(playerPed) > 100 then
@@ -26,33 +25,52 @@ function tvRP.startCheatCheck()
 				end
 
 				-- God mode check (invinsible flag)
-				if not isAdmin then
-		      		if not tvRP.isInPrison() and not tvRP.isInComa() then
-				      	if (GetPlayerInvincible(playerid)) then
-				        	SetEntityInvincible(playerPed, false)
-				        	SetPlayerInvincible(playerid, false)
-				        	incrementBanTrigger()
-				        	if runTimer and banTriggerCount > maxTrigger then
-				        		TriggerServerEvent("anticheat:ban", "Player invinsible state detected "..banTriggerCount.."+ times in 30 seconds. Auto ban applied")
-				        	else
-				        		TriggerServerEvent("anticheat:log", "Player invinsible state detected", "Godmode ban. Godmode set by player")
-				        	end
-				        end
-			      	end
-			    end
-
-			    --Invisible check
-			    if not isAdmin then
-			      	if not IsEntityVisible(playerPed) and tvRP.isCheckDelayed() < 1 and not tvRP.getDriveTestStatus() then
-			        	SetEntityVisible(playerPed, true, false)
+	      		if not tvRP.isInPrison() and not tvRP.isInComa() then
+			      	if (GetPlayerInvincible(playerid)) then
+			        	SetEntityInvincible(playerPed, false)
+			        	SetPlayerInvincible(playerid, false)
 			        	incrementBanTrigger()
 			        	if runTimer and banTriggerCount > maxTrigger then
-			        		TriggerServerEvent("anticheat:ban", "Player not visible detected "..banTriggerCount.."+ times in 30 seconds. Auto ban applied")
+			        		TriggerServerEvent("anticheat:ban", "Player invinsible state detected "..banTriggerCount.."+ times in 30 seconds. Auto ban applied")
 			        	else
-			        		TriggerServerEvent("anticheat:log", "Player not visible detected", "Invisible/NoClip ban. Action set by player")
+			        		TriggerServerEvent("anticheat:log", "Player invinsible state detected", "Godmode ban. Godmode set by player")
 			        	end
 			        end
-			    end
+		      	end
+
+			    --Invisible check
+		      	if not IsEntityVisible(playerPed) and tvRP.isCheckDelayed() < 1 and not tvRP.getDriveTestStatus() then
+		        	SetEntityVisible(playerPed, true, false)
+		        	incrementBanTrigger()
+		        	if runTimer and banTriggerCount > maxTrigger then
+		        		TriggerServerEvent("anticheat:ban", "Player not visible detected "..banTriggerCount.."+ times in 30 seconds. Auto ban applied")
+		        	else
+		        		TriggerServerEvent("anticheat:log", "Player not visible detected", "Invisible/NoClip ban. Action set by player")
+		        	end
+		        end
+
+		        -- Prevent unlimited ammo
+		        SetPedInfiniteAmmoClip(PlayerPedId(), false)
+		    end
+		end)
+		Citizen.CreateThread(function()
+			Citizen.Wait(30000)
+		    while threadStarted do
+		    	Citizen.Wait(60000)
+
+				if not tvRP.isInPrison() and not tvRP.isInComa() then
+					local curPed = PlayerPedId()
+					local curHealth = GetEntityHealth( curPed )
+					SetEntityHealth( curPed, curHealth-2)
+					-- this will substract 2hp from the current player, wait 50ms and then add it back, this is to check for hacks that force HP at 200
+					Citizen.Wait(50)
+
+					if PlayerPedId() == curPed and GetEntityHealth(curPed) == curHealth and GetEntityHealth(curPed) ~= 0 then
+						TriggerServerEvent("anticheat:ban", "Player health regen/force health script detected. Auto ban applied")
+					elseif GetEntityHealth(curPed) == curHealth-2 then
+						SetEntityHealth(curPed, GetEntityHealth(curPed)+2)
+					end
+				end
 		    end
 		end)
 	end
