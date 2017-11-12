2291e050a2a9b9ef359c0171f499b7c4b0ddee49
Serpico <bamboozol@gmail.com>
2017-11-12 18:47:22 -0500

    Prevent seat shuffling

--- resources/vrp/client/player_state.lua
+++ resources/vrp/client/player_state.lua
@@ -527,3 +527,16 @@ Citizen.CreateThread( function()
     Citizen.Wait(0)
   end
 end)
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+    if IsPedInAnyVehicle(GetPlayerPed(-1), false) then
+      if GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false), 0) == GetPlayerPed(-1) then
+        if GetIsTaskActive(GetPlayerPed(-1), 165) then
+          SetPedIntoVehicle(GetPlayerPed(-1), GetVehiclePedIsIn(GetPlayerPed(-1), false), 0)
+        end
+      end
+    end
+  end
+end)
