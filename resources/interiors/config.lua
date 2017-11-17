3ddcf48dc1270a18194f0506c4b926c38eb9cd6b
Serpico <bamboozol@gmail.com>
2017-11-17 00:49:01 -0500

    Added to for la fuerta blance house

--- resources/interiors/config.lua
+++ resources/interiors/config.lua
@@ -7,6 +7,10 @@ INTERIORS = {
     [4] = {id = 4, x = 138.18788146973, y = -764.86633300781, z = 45.75199508667,  name = "FIB Lobby", destination = {5}},
     [5] = {id = 5, x = 136.24076843262, y = -761.77978515625, z = 242.15208435058,  name = "FIB Office", destination = {4}},
 
+    --La Fuente Blanca
+    [6] = {id = 6, x = 1394.3980712891, y = 1141.6976318359, z = 114.60711669922,  name = "Enter House", destination = {7}},
+    [7] = {id = 7, x = 1396.7640380859, y = 1141.7806396484, z = 114.33378601074,  name = "Exit House", destination = {6}},
+
     -- BAHMAS MAMAS
     [8] = {id = 8, x = -1388.9272460938, y = -586.08123779297, z = 30.219741821289, h = 35.407787322998, name = "Exit Bahama Mamas", destination = {9}},
     [9] = {id = 9, x = -1394.5030517578, y = -596.09210205078, z = 30.319562911987, h = 205.4248046875, name = "Enter Bahama Mamas", destination = {8}},
@@ -18,4 +22,5 @@ INTERIORS = {
 	-- Humane labs
     [18] = {id = 18, x =  3562.8259277344, y = 3690.3332519531, z = 28.121658325195,  name = "Enter Lab", destination = {19}},
     [19] = {id = 19,x = 3526.3579101563, y = 3673.7409667969, z = 28.121139526367,  name = "Exit Exit", destination = {18}},
+
 }
