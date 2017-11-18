6d407e9f756c143c23b2a626c51e729c63c64b02
Serpico <bamboozol@gmail.com>
2017-11-17 21:23:25 -0500

    Revert "Added pNotify into vrp. Hoping this will be the fix for notifications failing"
    This reverts commit d92d7463ce268ce5a512ec98e2e9922207857c8b.

--- resources/vrp/gui/pNotify.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    window.addEventListener("message", function(event){
-        if(event.data.options){
-          var options = event.data.options;
-          new Noty(options).show()
-        };
-    });
-});
