85b921b231b5ecb2c19592a1f9a22410ff37cead
Serpico <bamboozol@gmail.com>
2017-11-17 21:23:25 -0500

    Revert "Added pNotify into vrp. Hoping this will be the fix for notifications failing"
    This reverts commit 498321005c5b4b802d10b60713fb67ba04b56b21.

--- resources/vrp/client/base.lua
+++ resources/vrp/client/base.lua
@@ -148,55 +148,7 @@ function tvRP.notify(msg, alert)
     PlaySoundFrontend(-1, "On_Call_Player_Join", "DLC_HEISTS_GENERAL_FRONTEND_SOUNDS", 1)
   end
 
-  VrpSendNotification({text = msg , type = "success", layout = "centerLeft", queue = "left", theme = "gta", timeout = 5000})
-
-  --TriggerEvent("pNotify:SendNotification", {text = msg , type = "success", layout = "centerLeft", queue = "left", theme = "gta", timeout = math.random(1000, 10000)})
-end
-
-function VrpSetQueueMax(queue, max)
-    local tmp = {
-        queue = tostring(queue),
-        max = tonumber(max)
-    }
-
-    SendNUIMessage({maxNotifications = tmp})
-end
-
-function VrpSendNotification(options)
-    options.animation = options.animation or {}
-    options.sounds = options.sounds or {}
-    options.docTitle = options.docTitle or {}
-
-    local options = {
-        type = options.type or "success",
-        layout = options.layout or "topRight",
-        theme = options.theme or "gta",
-        text = options.text or "Empty Notification",
-        timeout = options.timeout or 5000,
-        progressBar = options.progressBar or true,
-        closeWith = options.closeWith or {},
-        animation = {
-            open = options.animation.open or "gta_effects_open",
-            close = options.animation.close or "gta_effects_close"
-        },
-        sounds = {
-            volume = options.sounds.volume or 1,
-            conditions = options.sounds.conditions or {},
-            sources = options.sounds.sources or {}
-        },
-        docTitle = {
-            conditions = options.docTitle.conditions or {}
-        },
-        modal = options.modal or false,
-        id = options.id or false,
-        force = options.force or false,
-        queue = options.queue or "global",
-        killer = options.killer or false,
-        container = options.container or false,
-        buttons = options.button or false
-    }
-
-    SendNUIMessage({options = options})
+  TriggerEvent("pNotify:SendNotification", {text = msg , type = "success", layout = "centerLeft", queue = "left", theme = "gta", timeout = math.random(1000, 10000)})
 end
 
 -- Displays a subtitled mission text, like single player, bottom center of screen
