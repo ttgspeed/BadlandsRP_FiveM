d1f6d45a5cff5f2456fee6c683b0403a830e240d
Serpico <bamboozol@gmail.com>
2017-11-24 02:17:37 -0500

    Added hand on radio animation and sound. Bound to Z (GTA default binding Radar Zoom/Multiplayer Information)

--- resources/vrp/client/police.lua
+++ resources/vrp/client/police.lua
@@ -706,3 +706,50 @@ end
 function tvRP.setArmour(amount)
   SetPedArmour(GetPlayerPed(-1),amount)
 end
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait( 0 )
+    local ped = PlayerPedId()
+    if DoesEntityExist( ped ) and not IsEntityDead( ped ) and tvRP.isCop() then
+      if not IsPauseMenuActive() then
+        loadAnimDict( "random@arrests" )
+        if IsControlJustReleased( 0, 20 ) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+          TriggerServerEvent('InteractSound_SV:PlayOnSource', 'off', 0.05)
+          ClearPedTasks(ped)
+          SetEnableHandcuffs(ped, false)
+        else
+          if IsControlJustPressed( 0, 20 ) and not IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+            TriggerServerEvent('InteractSound_SV:PlayOnSource', 'on', 0.05)
+            TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
+            SetEnableHandcuffs(ped, true)
+          elseif IsControlJustPressed( 0, 20 ) and IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+            TriggerServerEvent('InteractSound_SV:PlayOnSource', 'on', 0.05)
+            TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
+            SetEnableHandcuffs(ped, true)
+          end
+          if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
+            DisableActions(ped)
+          elseif IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "radio_chatter", 3) then
+            DisableActions(ped)
+          end
+        end
+      end
+    end
+  end
+end )
+
+function loadAnimDict( dict )
+  while ( not HasAnimDictLoaded( dict ) ) do
+    RequestAnimDict( dict )
+    Citizen.Wait( 0 )
+  end
+end
+
+function DisableActions(ped)
+  DisableControlAction(1, 140, true)
+  DisableControlAction(1, 141, true)
+  DisableControlAction(1, 142, true)
+  DisableControlAction(1, 37, true) -- Disables INPUT_SELECT_WEAPON (TAB)
+  DisablePlayerFiring(ped, true) -- Disable weapon firing
+end
