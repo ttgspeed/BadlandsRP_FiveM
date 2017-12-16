827154aa204ec7f275452d827187dbd355ccadf6
Serpico <bamboozol@gmail.com>
2017-12-16 01:08:48 -0500

    Prevent vehicle weapon reward exploit

--- resources/CustomScripts/server.lua
+++
@@ -1,6 +0,0 @@
-AddEventHandler('chatMessage', function(source, name, message)
-	if message:sub(1,6) == '/phone' then
-		TriggerClientEvent('camera:phone', source, message)
-		CancelEvent()
-	end
-end)
