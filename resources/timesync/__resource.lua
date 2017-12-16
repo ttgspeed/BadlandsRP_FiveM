3bf54e4d634946a0209ae79f6b2a081713ecc01a
Serpico <bamboozol@gmail.com>
2017-12-16 03:12:21 -0500

    Better timesync

--- resources/timesync/__resource.lua
+++ resources/timesync/__resource.lua
@@ -1,4 +1,6 @@
-resource_manifest_version "77731fab-63ca-442c-a67b-abc70f28dfa5"
+--- Source https://github.com/TomGrobbe/vSync
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 server_scripts {
 		"server/time.lua",
