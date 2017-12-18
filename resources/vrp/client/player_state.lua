31e88fffdfeb914650b86bf4765d40b6065edf4d
Serpico <bamboozol@gmail.com>
2017-12-17 19:47:13 -0500

    Only allow engine toggle while in drivers seat. Holding G while in passenger seat will make you shuffle seats

--- resources/vrp/client/player_state.lua
+++ resources/vrp/client/player_state.lua
@@ -616,12 +616,13 @@ Citizen.CreateThread( function()
   end
 end)
 
+-- Prevent player from shuffling seats
 Citizen.CreateThread(function()
   while true do
     Citizen.Wait(0)
     if IsPedInAnyVehicle(GetPlayerPed(-1), false) then
       if GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false), 0) == GetPlayerPed(-1) then
-        if GetIsTaskActive(GetPlayerPed(-1), 165) then
+        if GetIsTaskActive(GetPlayerPed(-1), 165) and not IsControlPressed(0,47) then
           SetPedIntoVehicle(GetPlayerPed(-1), GetVehiclePedIsIn(GetPlayerPed(-1), false), 0)
         end
       end
