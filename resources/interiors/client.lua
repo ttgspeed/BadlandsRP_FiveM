17a68cde7b4088c41518cf60476e9aa364d2c3a6
Serpico <bamboozol@gmail.com>
2018-01-28 23:20:00 -0500

    Change markers to flat circle.

--- resources/interiors/client.lua
+++ resources/interiors/client.lua
@@ -29,7 +29,7 @@ Citizen.CreateThread(function()
         local playerPos = GetEntityCoords(ped, true)
 
         for i,pos in pairs(INTERIORS) do
-            DrawMarker(1, pos.x, pos.y, pos.z-1, 0, 0, 0, 0, 0, 0, 1.0,1.0,0.5, 255,255,255, 200, 0, 0, 2, 0, 0, 0, 0)
+            DrawMarker(23, pos.x, pos.y, pos.z-1, 0, 0, 0, 0, 0, 0, 1.0,1.0,0.5, 255,255,255, 200, 0, 0, 2, 0, 0, 0, 0)
             if (Vdist(playerPos.x, playerPos.y, playerPos.z, pos.x, pos.y, pos.z) < 1.0) and (not PED_hasBeenTeleported) then
                 POS_actual = pos.id
                 if not gui_interiors.opened then
