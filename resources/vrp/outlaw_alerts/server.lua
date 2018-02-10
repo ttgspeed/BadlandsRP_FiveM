19a0cd640a5d5da311608ad5e9113ebd322f9736
Serpico <bamboozol@gmail.com>
2018-02-10 17:06:05 -0500

    Fix formatting in server file for alerts. Fixed alerts not triggering.

--- resources/vrp/outlaw_alerts/server.lua
+++ resources/vrp/outlaw_alerts/server.lua
@@ -8,14 +8,14 @@ local service = services["Police"]
 RegisterServerEvent('thiefInProgress')
 AddEventHandler('thiefInProgress', function(street1, street2, veh, sex)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
+  end
+  for k,v in pairs(players) do
 		if veh == "NULL" then
 			TriggerClientEvent("outlawNotify", v, "Thief of a vehicle by a "..sex.." between "..street1.." and "..street2)
 		else
@@ -27,33 +27,33 @@ end)
 RegisterServerEvent('thiefInProgressS1')
 AddEventHandler('thiefInProgressS1', function(street1, veh, sex)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
-		if veh == "NULL" then
-			TriggerClientEvent("outlawNotify", v, "Thief of a vehicle by a "..sex.." at "..street1)
-		else
-			TriggerClientEvent("outlawNotify", v, "Thief of a "..veh.." by a "..sex.." at "..street1)
-		end
+  end
+  for k,v in pairs(players) do
+  	if veh == "NULL" then
+  		TriggerClientEvent("outlawNotify", v, "Thief of a vehicle by a "..sex.." at "..street1)
+  	else
+  		TriggerClientEvent("outlawNotify", v, "Thief of a "..veh.." by a "..sex.." at "..street1)
+  	end
 	end
 end)
 
 RegisterServerEvent('meleeInProgress')
 AddEventHandler('meleeInProgress', function(street1, street2, sex)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
+  end
+  for k,v in pairs(players) do
 		TriggerClientEvent("outlawNotify", v, "Fight initiated by a "..sex.." between "..street1.." and "..street2)
 	end
 end)
@@ -61,14 +61,14 @@ end)
 RegisterServerEvent('meleeInProgressS1')
 AddEventHandler('meleeInProgressS1', function(street1, sex)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
+  end
+  for k,v in pairs(players) do
 		TriggerClientEvent("outlawNotify", v, "Fight initiated by a "..sex.." at "..street1)
 	end
 end)
@@ -77,14 +77,14 @@ end)
 RegisterServerEvent('gunshotInProgress')
 AddEventHandler('gunshotInProgress', function(street1, street2, sex)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
+  end
+  for k,v in pairs(players) do
 		TriggerClientEvent("outlawNotify", v, "Gunshot by a "..sex.." between "..street1.." and "..street2)
 	end
 end)
@@ -92,14 +92,14 @@ end)
 RegisterServerEvent('gunshotInProgressS1')
 AddEventHandler('gunshotInProgressS1', function(street1, sex)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
+  end
+  for k,v in pairs(players) do
 		TriggerClientEvent("outlawNotify", v, "Gunshot by a "..sex.." at "..street1)
 	end
 end)
@@ -107,44 +107,44 @@ end)
 RegisterServerEvent('thiefInProgressPos')
 AddEventHandler('thiefInProgressPos', function(tx, ty, tz)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
-		TriggerClientEvent('thiefPlace', v, tx, ty, tz)
+  end
+  for k,v in pairs(players) do
+	 TriggerClientEvent('thiefPlace', v, tx, ty, tz)
 	end
 end)
 
 RegisterServerEvent('gunshotInProgressPos')
 AddEventHandler('gunshotInProgressPos', function(gx, gy, gz)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
-		TriggerClientEvent('gunshotPlace', v, gx, gy, gz)
+  end
+  for k,v in pairs(players) do
+	 TriggerClientEvent('gunshotPlace', v, gx, gy, gz)
 	end
 end)
 
 RegisterServerEvent('meleeInProgressPos')
 AddEventHandler('meleeInProgressPos', function(mx, my, mz)
 	local players = {}
-    for k,v in pairs(vRP.rusers) do
-      local player = vRP.getUserSource(tonumber(k))
-      -- check user
-      if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
-        table.insert(players,player)
-      end
+  for k,v in pairs(vRP.rusers) do
+    local player = vRP.getUserSource(tonumber(k))
+    -- check user
+    if vRP.hasPermission(k,service.alert_permission) and player ~= nil then
+      table.insert(players,player)
     end
-    for k,v in pairs(players) do
-		TriggerClientEvent('meleePlace', v, mx, my, mz)
+  end
+  for k,v in pairs(players) do
+	 TriggerClientEvent('meleePlace', v, mx, my, mz)
 	end
 end)
