978567a1124dadfe281718ac7457e61ce1e9df8d
Serpico <bamboozol@gmail.com>
2018-02-20 19:28:55 -0500

    No paychecks while in prison or jailed

--- resources/vrp/client/paycheck.lua
+++ resources/vrp/client/paycheck.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function ()
 	while true do
 		Citizen.Wait(300000) -- Every X ms you'll get paid (300000 = 5 min)
-		if not tvRP.isInComa() then
+		if not tvRP.isInComa() and not tvRP.isInPrison() and not tvRP.isJailed() then
 			TriggerServerEvent('vRP:salary')
 		end
 	end
