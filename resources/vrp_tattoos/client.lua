90aa2b622d0940c7c14dfb1288b2184202774b92
Serpico <bamboozol@gmail.com>
2018-02-27 23:03:30 -0500

    Buzz buzz, tattoo gun sound

--- resources/vrp_tattoos/client.lua
+++ resources/vrp_tattoos/client.lua
@@ -4,8 +4,10 @@ TSserver = Tunnel.getInterface("vrp_tattoos","vrp_tattoos")
 
 function vRPts.cleanPlayer()
 	ClearPedDecorations(GetPlayerPed(-1))
+	PlaySoundFrontend(-1, "Tattooing_Oneshot_Remove", "TATTOOIST_SOUNDS", 1)
 end
 
 function vRPts.drawTattoo(tattoo,tattooshop)
 	SetPedDecoration(GetPlayerPed(-1), GetHashKey(tattooshop), GetHashKey(tattoo))
+	PlaySoundFrontend(-1, "Tattooing_Oneshot", "TATTOOIST_SOUNDS", 1)
 end
