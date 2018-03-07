ca4c6cdb4aaca2f0c3ac0d6da9cabf77d926476e
Serpico <bamboozol@gmail.com>
2018-03-07 01:22:14 -0500

    Prevent going over inventory limit

--- resources/vrp/modules/farming.lua
+++ resources/vrp/modules/farming.lua
@@ -1,6 +1,11 @@
 function tvRP.giveFarmingReward(reward,ammount)
 	local user_id = vRP.getUserId(source)
 	if reward ~= nil and user_id ~= nil and ammount ~= nil then
-		vRP.giveInventoryItem(user_id,reward,ammount,true)
+		if (vRP.getInventoryWeight(user_id)+1) <= vRP.getInventoryMaxWeight(user_id) then
+			vRP.giveInventoryItem(user_id,reward,ammount,false)
+		else
+			vRP.giveInventoryItem(user_id,"cannabis_seed",1,false)
+			vRPclient.notify(source,{"Inventory full. Cannabis seed returned."})
+		end
 	end
 end
