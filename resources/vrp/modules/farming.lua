fd079195531ccabc506b8478bf316e29ec473262
Serpico <bamboozol@gmail.com>
2018-03-08 00:17:13 -0500

    Added iZones. Allows to create zones of any shape. Weed farming restricted to these zones.

--- resources/vrp/modules/farming.lua
+++ resources/vrp/modules/farming.lua
@@ -1,4 +1,5 @@
 function tvRP.giveFarmingReward(reward,ammount)
+	local source = source
 	local user_id = vRP.getUserId(source)
 	if reward ~= nil and user_id ~= nil and ammount ~= nil then
 		if (vRP.getInventoryWeight(user_id)+1) <= vRP.getInventoryMaxWeight(user_id) then
