9ee2ce4a464224d40f1e1112f613cda1bff381d2
Serpico <bamboozol@gmail.com>
2018-03-14 00:05:06 -0400

    Admin teleport to waypoint will use mellotrainer method to set player on ground properly

--- resources/vrp/client/admin.lua
+++ resources/vrp/client/admin.lua
@@ -58,15 +58,44 @@ function tvRP.isAdmin()
   return admin
 end
 
+-- Teleport to waypoint is from mellotrainer
 function tvRP.teleportWaypoint()
-  local playerPed = GetPlayerPed(-1)
-  local WaypointHandle = GetFirstBlipInfoId(8)
-  if DoesBlipExist(WaypointHandle) then
-    local coord = Citizen.InvokeNative(0xFA7C7F0AADF25D09, WaypointHandle, Citizen.ResultAsVector())
-    tvRP.teleport(coord.x,coord.y,coord.z)
-  else
-    tvRP.notify("No waypoint set.")
+  local targetPed = GetPlayerPed(-1)
+  if(IsPedInAnyVehicle(targetPed))then
+    targetPed = GetVehiclePedIsUsing(targetPed)
   end
+
+  if(not IsWaypointActive())then
+    return
+  end
+
+  local waypointBlip = GetFirstBlipInfoId(8) -- 8 = Waypoint ID
+  local x,y,z = table.unpack(Citizen.InvokeNative(0xFA7C7F0AADF25D09, waypointBlip, Citizen.ResultAsVector()))
+
+  -- Ensure Entity teleports above the ground
+  local ground
+  local groundFound = false
+  local groundCheckHeights = {0.0, 50.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0,450.0, 500.0, 550.0, 600.0, 650.0, 700.0, 750.0, 800.0}
+
+
+  for i,height in ipairs(groundCheckHeights) do
+    RequestCollisionAtCoord(x, y, height)
+    Wait(0)
+    SetEntityCoordsNoOffset(targetPed, x,y,height, 0, 0, 1)
+    ground,z = GetGroundZFor_3dCoord(x,y,height)
+    if(ground) then
+      z = z + 3
+      groundFound = true
+      break;
+    end
+  end
+
+  if(not groundFound)then
+    z = 1000
+    GiveDelayedWeaponToPed(PlayerPedId(), 0xFBAB5776, 1, 0) -- Parachute
+  end
+
+  SetEntityCoordsNoOffset(targetPed, x,y,z, 0, 0, 1)
 end
 
 -- noclip/invisibility
