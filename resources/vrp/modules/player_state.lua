b547d73455c7cdb768adecb947516513ea8eea8b
Serpico <bamboozol@gmail.com>
2018-03-18 21:04:09 -0400

    Fix issue with default voip not being set.

--- resources/vrp/modules/player_state.lua
+++ resources/vrp/modules/player_state.lua
@@ -266,6 +266,7 @@ AddEventHandler("vRP:playerSpawn",function(user_id,source,first_spawn)
   if first_spawn then
     -- We dont use vRP garages
     build_client_lockers(source)
+    TriggerClientEvent('setInitialVoice', source)
   end
 end)
 
