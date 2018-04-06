153b70aab7baad2a43d09b21492ed9016ff53d02
Serpico <bamboozol@gmail.com>
2018-04-05 20:23:27 -0400

    fix car spawn error. TEMP disabled pre req to practical test.

--- resources/dmvschool/client.lua
+++ resources/dmvschool/client.lua
@@ -110,9 +110,9 @@ function startttest()
 end
 
 function startptest()
-  if test_stage < 2 then
+  --if test_stage < 2 then
     DrawMissionText2("~r~You must complete the Driving Introduction and Practical Test first.", 5000)
-  else
+  --else
     TriggerServerEvent('dmv:dtcharge')
     onTestBlipp = AddBlipForCoord(255.13990783691, - 1400.7319335938, 30.5374584198)
     N_0x80ead8e2e1d5d52e(onTestBlipp)
@@ -121,7 +121,7 @@ function startptest()
     SpeedControl = 1
     TriggerEvent("vrp:driverteststatus", true)
     DTut()
-  end
+  --end
 end
 
 function EndDTest()
@@ -190,35 +190,34 @@ function SpawnTestCar()
   Citizen.Wait(0)
   local myPed = GetPlayerPed(-1)
   local player = PlayerId()
-  local vehicle = GetHashKey('dilettante')
+  local vehicleHash = GetHashKey('dilettante')
 
   local i = 0
-  while not HasModelLoaded(vehicle) and i < 10000 do
-    RequestModel(vehicle)
+  while not HasModelLoaded(vehicleHash) and i < 10000 do
+    RequestModel(vehicleHash)
     Citizen.Wait(10)
     i = i+1
   end
 
-  if HasModelLoaded(vehicle) then
+  if HasModelLoaded(vehicleHash) then
     SetEntityVisible(myPed, true)
     FreezeEntityPosition(myPed, false)
-    colors = table.pack(GetVehicleColours(vehicle))
-    extra_colors = table.pack(GetVehicleExtraColours(vehicle))
-    plate = math.random(100, 900)
-    spawned_car = CreateVehicle(vehicle, 249.40971374512, -1407.2303466797, 30.409454345703, 0.0, true, false)
+    spawned_car = CreateVehicle(vehicleHash, 249.40971374512, -1407.2303466797, 30.409454345703, 0.0, true, false)
+    colors = table.pack(GetVehicleColours(spawned_car))
+    extra_colors = table.pack(GetVehicleExtraColours(spawned_car))
     SetVehicleDoorsLocked(spawned_car,2)
     SetVehicleColours(spawned_car, 4, 5)
     SetVehicleExtraColours(spawned_car, extra_colors[1], extra_colors[2])
     SetEntityHeading(spawned_car, 317.64)
     SetVehicleOnGroundProperly(spawned_car)
     SetPedIntoVehicle(myPed, spawned_car, - 1)
-    SetModelAsNoLongerNeeded(vehicle)
+    SetModelAsNoLongerNeeded(vehicleHash)
     Citizen.InvokeNative(0xB736A491E64A32CF, Citizen.PointerValueIntInitialized(spawned_car))
     in_test_vehicle = 1
     TriggerEvent("pNotify:SendNotification", {
       text = "Turn engine on. (G is default)",
       type = "alert",
-      timeout = (5000),
+      timeout = 5000,
       layout = "center",
       queue = "global"
     })
@@ -241,7 +240,7 @@ function DIntro()
   TriggerEvent("pNotify:SendNotification", {
     text = "<b style='color:#1E90FF'>Driving Introduction</b> <br /><br />This introduction will cover the basics and ensure you are prepared with enough information and knowledge for your test.<br /><br />The information from your theory lessons combined with the experience from your practical lesson are vital for negotiating the situations and dilemmas you will face on the road.<br /><br />It is highly recommended that you pay attention to every detail, as you will be tested later on.",
     type = "alert",
-    timeout = (25000),
+    timeout = 25000,
     layout = "center",
     queue = "global"
   })
@@ -250,7 +249,7 @@ function DIntro()
   TriggerEvent("pNotify:SendNotification", {
     text = "<b style='color:#1E90FF'>Accidents and environmental concerns</b><br /><br /><b style='color:#87CEFA'>Duty to yield</b><br />All drivers have a duty to obey the rules of the road and avoid intentional harm to others.<br /><br /> When you hear an emergency vehicle, you should yield and pull over to your right.<br />Failure to stop for the police will result in your driver license being revoked, as well as a fine and/or jail time.<br /><br /><b style='color:#87CEFA'>Aggressive Driving</b><br />Intentionally hitting pedestrians or other vehicles with your car is not allowed under any circumstances.<br />Driving aggressively for the sake of inciting a police chase is not allowed.<br />",
     type = "alert",
-    timeout = (25000),
+    timeout = 25000,
     layout = "center",
     queue = "global"
   })
@@ -259,7 +258,7 @@ function DIntro()
   TriggerEvent("pNotify:SendNotification", {
     text = "<b style='color:#1E90FF'>Reckless Driving</b> <br /><br />Reckless driving is defined as driving over the posted speed limit, and/or ignoring traffic regulations (stop signs, stop lights, etc)<br /><br />Reckless driving may result in a heavy fine, as well as your drivers license being revoked.<br />",
     type = "alert",
-    timeout = (25000),
+    timeout = 25000,
     layout = "center",
     queue = "global"
   })
@@ -268,7 +267,7 @@ function DIntro()
   TriggerEvent("pNotify:SendNotification", {
     text = "<b style='color:#1E90FF'>Speed Limits</b> <br /><br />The highway speed limit is 75mph. Traffic on motorways usually travels faster than on other roads, so you have less time to react, and less time to stop.<br /><br />The speed limit on surface streets is 50mph.<br />On surface streets, be sure to keep an eye out for pedestrians and other vehicles.<br />Obey all traffic regulations, including stop signs and red lights.<br />",
     type = "alert",
-    timeout = (25000),
+    timeout = 25000,
     layout = "center",
     queue = "global"
   })
@@ -277,7 +276,7 @@ function DIntro()
   TriggerEvent("pNotify:SendNotification", {
     text = "<b style='color:#1E90FF'>Drug Trafficking</b> <br /><br />Drugs are illegal in Los Santos!<br /><br />Transporting drugs may result in a heavy fine, as well as your vehicle being permanently seized by the police.<br />",
     type = "alert",
-    timeout = (25000),
+    timeout = 25000,
     layout = "center",
     queue = "global"
   })
@@ -286,7 +285,7 @@ function DIntro()
   TriggerEvent("pNotify:SendNotification", {
     text = "<b style='color:#1E90FF'>Alcohol</b> <br /><br />Drinking while driving is very dangerous. Alcohol and drugs impair your judgment, and how you react to sounds and what you see.<br /><br />Driving under the influence may result in seizure of your driver license, as well as jail time.<br />",
     type = "alert",
-    timeout = (25000),
+    timeout = 25000,
     layout = "center",
     queue = "global"
   })
@@ -308,7 +307,7 @@ function DTut()
   TriggerEvent("pNotify:SendNotification", {
     text = "<b style='color:#1E90FF'>Driving Instructor:</b> <br /><br /> We are currently preparing your vehicle for the test.<br /><br /><b style='color:#87CEFA'>Evaluation:</b><br />- Do not crash the vehicle or go over the posted speed limit. You will receive <b style='color:#A52A2A'>Error Points</b> whenever you fail to follow these rules<br /><br />- Too many <b style='color:#A52A2A'>Error Points</b> accumulated will result in a <b style='color:#A52A2A'>Failed</b> test",
     type = "alert",
-    timeout = (15000),
+    timeout = 16500,
     layout = "center",
     queue = "global"
   })
