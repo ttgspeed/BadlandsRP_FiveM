75869b15010b84504c27c64130093cae51f4b62c
ttgspeed <justin@stricker.io>
2018-04-20 16:54:42 -0400

    Admin spawn vehicles because admin abuse is fun. Also reenable aircraft engine state for airdrop missions.

--- resources/vrp/client/basic_garage.lua
+++ resources/vrp/client/basic_garage.lua
@@ -63,7 +63,6 @@ local emergency_vehicles = {
 }
 
 function tvRP.spawnGarageVehicle(vtype,name,options) -- vtype is the vehicle type (one vehicle per type allowed at the same time)
-
   local vehicle = vehicles[name]
   if vehicle and not IsVehicleDriveable(vehicle[3]) then -- precheck if vehicle is undriveable
     -- despawn vehicle
@@ -738,8 +737,6 @@ carblacklist = {
   -- Does not spawn and not used (more for shitter scripters)
   "PoliceOld1",
   "PoliceOld2",
-  "Marshall",
-  "Monster",
   "Technical",
   "Technical2",
   -- Gunrunning vehicles (dont naturally spawn)
@@ -1047,36 +1044,34 @@ Citizen.CreateThread(function()
     Citizen.Wait(0)
     veh = GetVehiclePedIsIn(GetPlayerPed(-1), false)
     if veh ~= nil then
-      if not IsThisModelAHeli(GetEntityModel(veh)) and not IsThisModelAPlane(GetEntityModel(veh)) then
-        if GetSeatPedIsTryingToEnter(GetPlayerPed(-1)) == -1 and not table.contains(engineVehicles, veh) then
-          table.insert(engineVehicles, {veh, IsVehicleEngineOn(veh)})
-        elseif IsPedInAnyVehicle(GetPlayerPed(-1), false) and not table.contains(engineVehicles, GetVehiclePedIsIn(GetPlayerPed(-1), false)) then
-          table.insert(engineVehicles, {GetVehiclePedIsIn(GetPlayerPed(-1), false), IsVehicleEngineOn(GetVehiclePedIsIn(GetPlayerPed(-1), false))})
-        end
-        for i, vehicle in ipairs(engineVehicles) do
-          if DoesEntityExist(vehicle[1]) then
-            if (GetPedInVehicleSeat(vehicle[1], -1) == GetPlayerPed(-1)) or IsVehicleSeatFree(vehicle[1], -1) then
-              if GetVehicleEngineHealth(vehicle[1]) >= 750 then
-                SetVehicleEngineOn(vehicle[1], vehicle[2], true, false)
-                SetVehicleJetEngineOn(vehicle[1], vehicle[2])
-              else
-                SetVehicleUndriveable(vehicle[1], true)
-              end
-              if not IsPedInAnyVehicle(GetPlayerPed(-1), false) or (IsPedInAnyVehicle(GetPlayerPed(-1), false) and vehicle[1]~= GetVehiclePedIsIn(GetPlayerPed(-1), false)) then
-                if IsThisModelAHeli(GetEntityModel(vehicle[1])) or IsThisModelAPlane(GetEntityModel(vehicle[1])) then
-                  if vehicle[2] then
-                    --SetHeliBladesFullSpeed(vehicle[1])
-                  end
+      if GetSeatPedIsTryingToEnter(GetPlayerPed(-1)) == -1 and not table.contains(engineVehicles, veh) then
+        table.insert(engineVehicles, {veh, IsVehicleEngineOn(veh)})
+      elseif IsPedInAnyVehicle(GetPlayerPed(-1), false) and not table.contains(engineVehicles, GetVehiclePedIsIn(GetPlayerPed(-1), false)) then
+        table.insert(engineVehicles, {GetVehiclePedIsIn(GetPlayerPed(-1), false), IsVehicleEngineOn(GetVehiclePedIsIn(GetPlayerPed(-1), false))})
+      end
+      for i, vehicle in ipairs(engineVehicles) do
+        if DoesEntityExist(vehicle[1]) then
+          if (GetPedInVehicleSeat(vehicle[1], -1) == GetPlayerPed(-1)) or IsVehicleSeatFree(vehicle[1], -1) then
+            if GetVehicleEngineHealth(vehicle[1]) >= 750 then
+              SetVehicleEngineOn(vehicle[1], vehicle[2], true, false)
+              SetVehicleJetEngineOn(vehicle[1], vehicle[2])
+            else
+              SetVehicleUndriveable(vehicle[1], true)
+            end
+            if not IsPedInAnyVehicle(GetPlayerPed(-1), false) or (IsPedInAnyVehicle(GetPlayerPed(-1), false) and vehicle[1]~= GetVehiclePedIsIn(GetPlayerPed(-1), false)) then
+              if IsThisModelAHeli(GetEntityModel(vehicle[1])) or IsThisModelAPlane(GetEntityModel(vehicle[1])) then
+                if vehicle[2] then
+                  --SetHeliBladesFullSpeed(vehicle[1])
                 end
               end
             end
-          else
-            table.remove(engineVehicles, i)
           end
+        else
+          table.remove(engineVehicles, i)
         end
-        if IsControlJustPressed(0, 47) and tvRP.isPedInCar() then
-          toggleEngine()
-        end
+      end
+      if IsControlJustPressed(0, 47) and tvRP.isPedInCar() then
+        toggleEngine()
       end
     end
   end
