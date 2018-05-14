fd445acd269071527e4c30b23858652537f68310
Serpico <bamboozol@gmail.com>
2018-05-14 19:50:11 -0400

    Nil check for splitstring

--- resources/vrp/client/impound.lua
+++ resources/vrp/client/impound.lua
@@ -41,13 +41,18 @@ function tvRP.impoundVehicleAtYard()
 			plate = GetVehicleNumberPlateText(vehicle)
 			if plate ~= nil and vehicle ~= nil then
 				args = tvRP.stringsplit(plate)
-				plate = args[1]
-				carModel = GetEntityModel(vehicle)
-				carName = GetDisplayNameFromVehicleModel(carModel)
+				if args ~= nil then
+					plate = args[1]
+					carModel = GetEntityModel(vehicle)
+					carName = GetDisplayNameFromVehicleModel(carModel)
 
-				SetEntityAsMissionEntity(vehicle,true,true)
-				SetVehicleAsNoLongerNeeded(Citizen.PointerValueIntInitialized(vehicle))
-				Citizen.InvokeNative(0xEA386986E786A54F, Citizen.PointerValueIntInitialized(vehicle))
+					SetEntityAsMissionEntity(vehicle,true,true)
+					SetVehicleAsNoLongerNeeded(Citizen.PointerValueIntInitialized(vehicle))
+					Citizen.InvokeNative(0xEA386986E786A54F, Citizen.PointerValueIntInitialized(vehicle))
+				else
+					tvRP.notify("Cannot impound while in a vehicle.")
+					return false, "", "", "","",""
+				end
 			end
 		end
 		-- check if the vehicle failed to impound. This happens if another player is nearby
@@ -74,20 +79,24 @@ function tvRP.tagNearestVehicleForTow(radius)
 	plate = GetVehicleNumberPlateText(vehicle)
 	if plate ~= nil and vehicle ~= nil then
 		args = tvRP.stringsplit(plate)
-		plate = args[1]
-		carModel = GetEntityModel(vehicle)
-		carName = GetDisplayNameFromVehicleModel(carModel)
-		if tvRP.getRegistrationNumber() == plate or tvRP.isCop() then
-			tvRP.notify("Vehicle tagged for towing")
-			SetVehicleDoorsLocked(vehicle,2)
-			SetVehicleDoorsLockedForAllPlayers(vehicle, true)
-			player = GetPlayerPed(-1)
-			coord = GetEntityCoords(player, true)
-			vRPserver.addToTowList({plate,carName,coord.x,coord.y,coord.z})
-			vRPserver.sendServiceAlert({GetPlayerServerId(PlayerId()),"Tow Truck",coord.x,coord.y,coord.z,"Vehicle marked for towing. Please bring to impound lot."})
-			SetEntityAsMissionEntity(vehicle,true,true)
+		if args ~= nil then
+			plate = args[1]
+			carModel = GetEntityModel(vehicle)
+			carName = GetDisplayNameFromVehicleModel(carModel)
+			if tvRP.getRegistrationNumber() == plate or tvRP.isCop() then
+				tvRP.notify("Vehicle tagged for towing")
+				SetVehicleDoorsLocked(vehicle,2)
+				SetVehicleDoorsLockedForAllPlayers(vehicle, true)
+				player = GetPlayerPed(-1)
+				coord = GetEntityCoords(player, true)
+				vRPserver.addToTowList({plate,carName,coord.x,coord.y,coord.z})
+				vRPserver.sendServiceAlert({GetPlayerServerId(PlayerId()),"Tow Truck",coord.x,coord.y,coord.z,"Vehicle marked for towing. Please bring to impound lot."})
+				SetEntityAsMissionEntity(vehicle,true,true)
+			else
+				tvRP.notify("You cannot tag someone else's vehicle for towing")
+			end
 		else
-			tvRP.notify("You cannot tag someone else's vehicle for towing")
+			tvRP.notify("No vehicle found")
 		end
 	else
 		tvRP.notify("No vehicle found")
