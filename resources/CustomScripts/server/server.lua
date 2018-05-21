b4b1d9263c1a2a65b22c74d8caf3ad878bb25e8a
Serpico <bamboozol@gmail.com>
2018-05-21 00:49:04 -0400

    Added ability for cops to change vehicle extras. Can do it to rank -1 vehicle.

--- resources/CustomScripts/server/server.lua
+++ resources/CustomScripts/server/server.lua
@@ -75,6 +75,19 @@ AddEventHandler('chatMessage', function(from,name,message)
 			if param ~= nil and action ~= nil then
 				TriggerClientEvent("CustomScripts:ToggleDoor", from, action, param)
 			end
+		elseif cmd == "/carmod" then
+			CancelEvent()
+			local extra = (tonumber(args[2]))
+			local toggle = (tonumber(args[3]))
+			if extra ~= nil and toggle ~= nil then
+				TriggerClientEvent("vRP:CarExtra", from, extra, toggle)
+			end
+		elseif cmd == "/carlivery" then
+			CancelEvent()
+			local value = (tonumber(args[2]))
+			if value ~= nil then
+				--TriggerClientEvent("vRP:CarLivery", from, value)
+			end
 		end
 	end
 end)
