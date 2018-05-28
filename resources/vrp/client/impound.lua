808a5f53352da22e0f5ba23cfc1066e387da1fbd
Serpico <bamboozol@gmail.com>
2018-05-28 02:25:10 -0400

    Replaced get closestobject with raycast. Player most always be looking at the vehicle.

--- resources/vrp/client/impound.lua
+++ resources/vrp/client/impound.lua
@@ -36,7 +36,7 @@ function tvRP.impoundVehicleAtYard()
 			Citizen.InvokeNative(0xEA386986E786A54F, Citizen.PointerValueIntInitialized(vehicle))
 		else
 			-- This is a backup to the impound. Mainly will be triggered for motorcyles and bikes
-			vehicle = tvRP.getNearestVehicle(5)
+			vehicle = tvRP.getVehicleAtRaycast(5)
 			plate = GetVehicleNumberPlateText(vehicle)
 			if plate ~= nil and vehicle ~= nil then
 				args = tvRP.stringsplit(plate)
@@ -74,7 +74,7 @@ function tvRP.impoundVehicleAtYard()
 end
 
 function tvRP.tagNearestVehicleForTow(radius)
-	vehicle = tvRP.getNearestVehicle(radius)
+	vehicle = tvRP.getVehicleAtRaycast(radius)
 	plate = GetVehicleNumberPlateText(vehicle)
 	if plate ~= nil and vehicle ~= nil then
 		args = tvRP.stringsplit(plate)
