dfbf108c7efd295e51280b5b040bf8128deb2959
ttgspeed <justin@stricker.io>
2018-06-02 00:27:17 -0400

    Spaces again

--- resources/vrp/modules/licenses.lua
+++ resources/vrp/modules/licenses.lua
@@ -20,78 +20,78 @@ clientaccess = Tunnel.getInterface("playerLicenses","playerLicenses") -- the sec
 
 RegisterServerEvent('vrp:purchaseLicense')
 AddEventHandler('vrp:purchaseLicense', function(license)
-  purchaseLicense(source, license)
-  return true
+	purchaseLicense(source, license)
+	return true
 end)
 
 RegisterServerEvent("vrp:driverSchoolPassed")
 AddEventHandler("vrp:driverSchoolPassed", function()
 	local user_id = vRP.getUserId(source)
-  MySQL.Async.execute('UPDATE vrp_user_identities SET driverschool = 1 WHERE user_id = @user_id', {user_id = user_id}, function(rowsChanged) end)
+	MySQL.Async.execute('UPDATE vrp_user_identities SET driverschool = 1 WHERE user_id = @user_id', {user_id = user_id}, function(rowsChanged) end)
 end)
 
 function purchaseLicense(player, license)
-  local user_id = vRP.getUserId(player)
+	local user_id = vRP.getUserId(player)
 
-  if license then
-    -- buy vehicle
-    local license_info = licenses[license]
-    playerLicenses.getPlayerLicense(user_id, license, function(license_owned)
-      if license_owned == -1 then
-        vRPclient.notify(player,{"The government has suspended your "..license_info[1].."; you may not obtain a new one."})
-      elseif license_owned == 1 then
-        vRPclient.notify(player,{"You already have a "..license_info[1]})
-      elseif license_owned then
-        vRP.request(player, "Do you want to buy "..license_info[1].." for $"..license_info[2], 15, function(player,ok)
-          if ok and vRP.tryFullPayment(user_id,license_info[2]) then
-            MySQL.Async.execute('UPDATE vrp_user_identities SET '..license_info[3]..' = 1 WHERE user_id = @user_id', {user_id = user_id}, function(rowsChanged) end)
-            vRPclient.notify(player,{"The state has issued you a "..license_info[1].." for $"..license_info[2]})
-            Log.write(user_id, "Purchased "..license_info[1].." for "..license_info[2], Log.log_type.purchase)
-          end
-        end)
-      else
-        vRPclient.notify(player,{lang.money.not_enough()})
-      end
-    end)
-  end
+	if license then
+		-- buy vehicle
+		local license_info = licenses[license]
+		playerLicenses.getPlayerLicense(user_id, license, function(license_owned)
+			if license_owned == -1 then
+				vRPclient.notify(player,{"The government has suspended your "..license_info[1].."; you may not obtain a new one."})
+			elseif license_owned == 1 then
+				vRPclient.notify(player,{"You already have a "..license_info[1]})
+			elseif license_owned then
+				vRP.request(player, "Do you want to buy "..license_info[1].." for $"..license_info[2], 15, function(player,ok)
+					if ok and vRP.tryFullPayment(user_id,license_info[2]) then
+						MySQL.Async.execute('UPDATE vrp_user_identities SET '..license_info[3]..' = 1 WHERE user_id = @user_id', {user_id = user_id}, function(rowsChanged) end)
+						vRPclient.notify(player,{"The state has issued you a "..license_info[1].." for $"..license_info[2]})
+						Log.write(user_id, "Purchased "..license_info[1].." for "..license_info[2], Log.log_type.purchase)
+					end
+				end)
+			else
+				vRPclient.notify(player,{lang.money.not_enough()})
+			end
+		end)
+	end
 end
 
 function playerLicenses.getPlayerLicenses(user_id, cbr)
-  local task = Task(cbr,{false})
-  local _plicenses = {}
-  MySQL.Async.fetchAll('SELECT driverschool,driverlicense,firearmlicense,pilotlicense,towlicense FROM vrp_user_identities WHERE user_id = @user_id', {user_id = user_id}, function(_plicenses)
-    task({_plicenses[1]})
-  end)
+	local task = Task(cbr,{false})
+	local _plicenses = {}
+	MySQL.Async.fetchAll('SELECT driverschool,driverlicense,firearmlicense,pilotlicense,towlicense FROM vrp_user_identities WHERE user_id = @user_id', {user_id = user_id}, function(_plicenses)
+		task({_plicenses[1]})
+	end)
 end
 
 function playerLicenses.getPlayerLicense(user_id, license, cbr)
-  local task = Task(cbr,{false})
+	local task = Task(cbr,{false})
 
-  playerLicenses.getPlayerLicenses(user_id, function(licenses)
+	playerLicenses.getPlayerLicenses(user_id, function(licenses)
 		if licenses ~= nil then
-	    for k,v in pairs(licenses) do
-	      if k == license then
-	        task({tonumber(v)})
-	      end
-	    end
+			for k,v in pairs(licenses) do
+				if k == license then
+					task({tonumber(v)})
+				end
+			end
 		else
 			task({0})
 		end
-  end)
+	end)
 end
 
 function playerLicenses.getPlayerLicenses_client(message)
-  local user_id = vRP.getUserId(source)
-  playerLicenses.getPlayerLicenses(user_id)
+	local user_id = vRP.getUserId(source)
+	playerLicenses.getPlayerLicenses(user_id)
 end
 
 function playerLicenses.getPlayerLicense_client(license)
-  local task = TUNNEL_DELAYED()
-  local user_id = vRP.getUserId(source)
+	local task = TUNNEL_DELAYED()
+	local user_id = vRP.getUserId(source)
 
-  playerLicenses.getPlayerLicense(user_id, license, function(has_license)
-    task({has_license})
-  end)
+	playerLicenses.getPlayerLicense(user_id, license, function(has_license)
+		task({has_license})
+	end)
 end
 
 vRP.playerLicenses = playerLicenses
