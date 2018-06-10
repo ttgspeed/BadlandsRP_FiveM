8042ce09e88a6c8d13e915673b56eb6cf2168aa8
Serpico <bamboozol@gmail.com>
2018-06-10 01:25:20 -0400

    Added seatbelt.... they workkkkkkk. also when buckled up, it disables F to exit

--- resources/vrp/client/player_actions.lua
+++ resources/vrp/client/player_actions.lua
@@ -141,3 +141,84 @@ function ProneMovement()
 		end
 	end
 end
+
+
+-- https://github.com/IndianaBonesUrMom/fivem-seatbelt
+local diffTrigger = 0.15
+local minSpeed = 19.25 --THIS IS IN m/s
+local strings = { belt_on = 'Seatbelt on.', belt_off = 'Seatbelt off' }
+local speedBuffer = {}
+local velBuffer = {}
+local beltOn = false
+local wasInCar = false
+
+IsCar = function(veh)
+		    local vc = GetVehicleClass(veh)
+		    return (vc >= 0 and vc <= 7) or (vc >= 9 and vc <= 12) or (vc >= 17 and vc <= 20)
+        end
+
+function Fwv(entity)
+  local hr = GetEntityHeading(entity) + 90.0
+  if hr < 0.0 then hr = 360.0 + hr end
+  hr = hr * 0.0174533
+  return { x = math.cos(hr) * 2.0, y = math.sin(hr) * 2.0 }
+end
+
+Citizen.CreateThread(function()
+	Citizen.Wait(500)
+	while true do
+
+		local ped = GetPlayerPed(-1)
+		local car = GetVehiclePedIsIn(ped)
+
+		if car ~= 0 then
+
+			--wasInCar = true
+
+			if beltOn then
+				DisableControlAction(0, 75)
+			end
+
+			if IsDisabledControlJustReleased(0,75) then
+				tvRP.notify("You need to remove your seatbelt to do that")
+			end
+
+			speedBuffer[2] = speedBuffer[1]
+			speedBuffer[1] = GetEntitySpeed(car)
+
+			if not tvRP.isHandcuffed() and speedBuffer[2] ~= nil
+			   and not beltOn
+			   and GetEntitySpeedVector(car, true).y > 1.0
+			   and speedBuffer[1] > minSpeed
+			   and (speedBuffer[2] - speedBuffer[1]) > (speedBuffer[1] * diffTrigger) then
+
+				Citizen.Trace("Eject")
+				local co = GetEntityCoords(ped)
+				local fw = Fwv(ped)
+				SetEntityCoords(ped, co.x + fw.x, co.y + fw.y, co.z - 0.47, true, true, true)
+				SetEntityVelocity(ped, velBuffer[2].x, velBuffer[2].y, velBuffer[2].z)
+				Citizen.Wait(1)
+				SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
+			end
+
+			velBuffer[2] = velBuffer[1]
+			velBuffer[1] = GetEntityVelocity(car)
+
+			if IsControlJustReleased(0, 311) then
+				beltOn = not beltOn
+				if beltOn then
+					tvRP.notify(strings.belt_on)
+				else
+					tvRP.notify(strings.belt_off)
+				end
+			end
+
+		--elseif wasInCar then
+		else
+			--wasInCar = false
+			beltOn = false
+			speedBuffer[1], speedBuffer[2] = 0.0, 0.0
+		end
+		Citizen.Wait(0)
+	end
+end)
