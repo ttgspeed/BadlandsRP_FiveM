eee2b599f43a05cb97c642906859834b1f02b47a
Serpico <bamboozol@gmail.com>
2018-06-26 00:38:01 -0400

    Translate and disable fixed positions for now

--- resources/gcphone/config.lua
+++ resources/gcphone/config.lua
@@ -1,7 +1,7 @@
 FixePhone = {
   -- Poste de police
-  ['911'] = { name =  "Central Police", coords = { x = 441.2, y = -979.7, z = 30.58 } },
-  
+  --['911'] = { name =  "Police HQ", coords = { x = 441.2, y = -979.7, z = 30.58 } },
+
   -- Cabine proche du poste de police
-  ['008-0001'] = { name = "Cabine Telephonique", coords = { x = 372.25, y = -965.75, z = 28.58 } },
+  --['008-0001'] = { name = "Telephone Booth", coords = { x = 372.25, y = -965.75, z = 28.58 } },
 }
