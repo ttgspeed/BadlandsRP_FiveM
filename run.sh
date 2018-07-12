9e9208df800034b08a0b8267e83c3b53a3c07292
ttgspeed <justin@stricker.io>
2018-07-12 09:51:15 -0400

    development environment updates

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /fivem-server/
+cd /fivem-server/development
 mv player.log playerlogs/player_$(date +"%Y-%m-%d_%H-%M-%S").log
 bash server/run.sh +exec server.cfg |& tee -a logs/server_$(date +"%Y-%m-%d_%H-%M-%S").log
