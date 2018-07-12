2bfc00d5f8d3bdadbff4874c584123a7008ac75b
ttgspeed <justin@stricker.io>
2018-07-12 09:51:15 -0400

    development environment updates

--- server/run.sh
+++ server/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # save the script directory
-# from https://stackoverflow.com/a/4774063/223967
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd`
-popd > /dev/null
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 
 echo "Removing Cache"
 #rm -rf $SCRIPTPATH/../cache
 find $SCRIPTPATH/../cache/files ! -regex  '.*\(sessionmanager\|files\)$' -print0 | xargs -0 rm -rf
 
-# run proot
-exec $SCRIPTPATH/proot -b $PWD -R $SCRIPTPATH/alpine/ /opt/cfx-server/FXServer +set citizen_dir /opt/cfx-server/citizen/ $* |& tee -a server.log
+# run server
+exec $SCRIPTPATH/alpine/opt/cfx-server/ld-musl-x86_64.so.1 \
+    --library-path "$SCRIPTPATH/alpine/usr/lib/v8/:$SCRIPTPATH/alpine/lib/:$SCRIPTPATH/alpine/usr/lib/" -- \
+    $SCRIPTPATH/alpine/opt/cfx-server/FXServer +set citizen_dir $SCRIPTPATH/alpine/opt/cfx-server/citizen/ $* |& tee -a server.log
