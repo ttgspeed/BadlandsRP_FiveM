ff1b823e65f785a404aa80be5cf345d9344210e2
Serpico <bamboozol@gmail.com>
2018-07-26 17:29:06 -0400

    Hospital revive only works if no medics online or if medic online (patient must be ems or lspd)

--- resources/vrp/modules/survival.lua
+++ resources/vrp/modules/survival.lua
@@ -241,18 +241,24 @@ menu["Send For Treatment"] = {function(player,choice)
   if user_id ~= nil then
     if vRP.hasPermission(user_id,"emergency.support") then
       vRPclient.getNearestPlayer(player,{5},function(nplayer)
-        local nuser_id = vRP.getUserId(nplayer)
-        if nuser_id ~= nil then
-          vRPclient.isInComa(nplayer,{}, function(in_coma)
-            if in_coma then
-              vRPhs.PutInBedServer({player, nplayer})
+        vRPclient.getMedicCount(player,{},function(medicCount)
+          if medicCount ~= nil and (medicCount < 1 or vRP.hasPermission(nplayer,"emergency.support")) then
+            local nuser_id = vRP.getUserId(nplayer)
+            if nuser_id ~= nil then
+              vRPclient.isInComa(nplayer,{}, function(in_coma)
+                if in_coma then
+                  vRPhs.PutInBedServer({player, nplayer})
+                else
+                  vRPclient.notify(player,{"No one needs treatment"})
+                end
+              end)
             else
-              vRPclient.notify(player,{"No one needs treatment"})
+              vRPclient.notify(player,{"No one near you need treatment."})
             end
-          end)
-        else
-          vRPclient.notify(player,{"No one near you need treatment."})
-        end
+          else
+            vRPclient.notify(player,{"The hospital is not accepting walk-ins, please call EMS for assistance."})
+          end
+        end)
       end)
     else
       vRPclient.notify(player,{"Hospital staff don't seem to recognize you."})
