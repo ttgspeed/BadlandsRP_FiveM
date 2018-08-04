24c808afe0739281dc3cd492350da4f001bfd5c3
Serpico <bamboozol@gmail.com>
2018-08-04 13:11:27 -0400

    Bahama doors and comedy club interior. NEED TP for club

--- resources/interiors/config.lua
+++ resources/interiors/config.lua
@@ -12,8 +12,8 @@ INTERIORS = {
   [7] = {id = 7, x = 1396.7640380859, y = 1141.7806396484, z = 114.33378601074,  name = "Enter House", destination = {6}},
 
   -- BAHMAS MAMAS
-  [8] = {id = 8, x = -1388.9272460938, y = -586.08123779297, z = 30.229741821289, h = 35.407787322998, name = "Exit Bahama Mamas", destination = {9}},
-  [9] = {id = 9, x = -1394.5030517578, y = -596.09210205078, z = 30.39562911987, h = 205.4248046875, name = "Enter Bahama Mamas", destination = {8}},
+  --[8] = {id = 8, x = -1388.9272460938, y = -586.08123779297, z = 30.229741821289, h = 35.407787322998, name = "Exit Bahama Mamas", destination = {9}},
+  --[9] = {id = 9, x = -1394.5030517578, y = -596.09210205078, z = 30.39562911987, h = 205.4248046875, name = "Enter Bahama Mamas", destination = {8}},
 
   --Sandy shore cells
   [10] = {id = 10, x = 1849.8493652344, y = 3683.3837890625, z = -118.76152038574, h = 35.407787322998, name = "Enter Cells", destination = {11}},
