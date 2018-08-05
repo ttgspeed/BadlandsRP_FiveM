71023493391c3aec8e2be16fcff57f624a53b2ac
ttgspeed <justin@stricker.io>
2018-08-05 12:19:08 -0400

    Add security cameras

---
+++ resources/securitycams/config.lua
@@ -0,0 +1,34 @@
+SecurityCamConfig = {}
+
+SecurityCamConfig.DebugMode = false
+SecurityCamConfig.HideRadar = true
+
+SecurityCamConfig.Locations = {
+    -- {
+    --     camBox = {label = "Pacific Standard Bank", x = 283.67, y = 264.41, z = 105.80},
+    --     cameras = {
+    --         {label = "Camera 1", x = 257.45, y = 210.07, z = 109.08, r = {x = -25.0, y = 0.0, z = 28.05}, canRotate = false},
+    --         {label = "Camera 2", x = 269.66, y = 223.67, z = 113.23, r = {x = -30.0, y = 0.0, z = 111.29}, canRotate = false},
+    --         {label = "Camera 3", x = 241.64, y = 233.83, z = 111.48, r = {x = -35.0, y = 0.0, z = 120.46}, canRotate = false},
+    --         {label = "Camera 4", x = 232.86, y = 221.46, z = 107.83, r = {x = -25.0, y = 0.0, z = -140.91}, canRotate = false},
+    --         {label = "Camera 5", x = 261.50, y = 218.08, z = 107.95, r = {x = -25.0, y = 0.0, z = -149.49}, canRotate = false},
+    --         {label = "Camera 6", x = 261.98, y = 217.92, z = 113.25, r = {x = -40.0, y = 0.0, z = -159.49}, canRotate = false},
+    --         {label = "Camera 7", x = 258.44, y = 204.97, z = 113.25, r = {x = -30.0, y = 0.0, z = 10.50}, canRotate = false},
+    --         {label = "Camera 8", x = 235.53, y = 227.37, z = 113.23, r = {x = -35.0, y = 0.0, z = -160.29}, canRotate = false},
+    --         {label = "Camera 9", x = 254.72, y = 206.06, z = 113.28, r = {x = -35.0, y = 0.0, z = 44.70}, canRotate = false},
+    --         {label = "Camera 10", x = 269.89, y = 223.76, z = 106.48, r = {x = -35.0, y = 0.0, z = 112.62}, canRotate = false},
+    --         {label = "Camera 11", x = 252.27, y = 225.52, z = 103.99, r = {x = -35.0, y = 0.0, z = -74.87}, canRotate = false},
+    --         {label = "Camera 12", x = 251.65, y = 218.66, z = 104.5, r = {x = 0.0, y = 0.0, z = -20.0}, canRotate = true}
+    --     },
+    --     allowedModels = {}
+    -- },
+		{
+        camBox = {label = "Cocaine Lab", x = 997.36108398438, y = -3195.6650390625, z = -37.39352798462},
+        cameras = {
+            {label = "Camera 1", x = 998.22552490234, y = -3193.7934570312, z = -34.878173828125, r = {x = -29.908912658692, y = -1.7075471987482, z = -137.52221679688}, canRotate = false},
+            {label = "Camera 2", x = 1009.193786621, y = -3201.4699707032, z = -35.067943572998, r = {x = -29.955417633056, y = 0.0, z = -6.8271822929382}, canRotate = true},
+            {label = "Camera 3", x = 1017.3354492188, y = -3193.7822265625, z = -35.092945098876, r = {x = -23.420419692994, y = 0.0, z = 129.31809997558}, canRotate = false},
+        },
+        allowedModels = {}
+    },
+}
