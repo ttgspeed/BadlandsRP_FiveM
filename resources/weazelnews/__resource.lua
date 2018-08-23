797e4293b9c005fb49e10cd4bfb03b81517e39b0
Serpico <bamboozol@gmail.com>
2018-08-23 03:39:29 -0400

    Added news job, more for RP. Sign in at weazel news and you get access to camera, mic, boom mic. Need to consider some type of alert maybe?

---
+++ resources/weazelnews/__resource.lua
@@ -0,0 +1,13 @@
+--[[
+    Scripted by: Xander Harrison [X. Cross]
+	Edited by: MrDiamondDirt
+--]]
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+dependency "vrp"
+
+server_scripts {
+  "@vrp/lib/utils.lua",
+  "server.lua",
+}
+client_script "client.lua"
