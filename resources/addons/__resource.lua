3ae96801ff98dbc48b4e5f5f996dbfc1cd97a0f9
Serpico <bamboozol@gmail.com>
2018-08-25 18:27:35 -0400

    Reduce fire delay on taser and increase range and dont kill players

--- resources/addons/__resource.lua
+++ resources/addons/__resource.lua
@@ -8,6 +8,7 @@ files {
 	'carcols.meta',
 	'handling.meta',
 	'vehicleaihandlinginfo.meta',
+	'weapons.meta',
 }
 
 -- specify data file entries to be added
@@ -17,3 +18,4 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'HANDLING_FILE' 'vehicleaihandlinginfo.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
