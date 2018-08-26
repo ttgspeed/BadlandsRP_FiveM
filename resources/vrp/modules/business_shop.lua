fae2470deb8c5b452ce6486023d47eb9c2dfedf9
ttgspeed <justin@stricker.io>
2018-08-26 18:24:14 -0400

    Shop permissions, dirty money, safe withdraw

--- resources/vrp/modules/business_shop.lua
+++ resources/vrp/modules/business_shop.lua
@@ -44,21 +44,23 @@ local function build_itemlist_menu(name, items, cb)
 end
 
 -- build the shop entry menu
-local function build_entry_menu(user_id, store_name)
+local function build_entry_menu(user_id, business_id, store_name)
 	local shop = cfg.stores[store_name]
 	local menu = {name=shop.name,css={top="75px",header_color="rgba(0,255,125,0.75)"}}
 
-	menu["Rent Shop"] = {function(player,choice)
-		vRP.getPlayerBusiness(user_id, function(business_id)
+	if shop.business == 0 then
+		menu["Rent Shop"] = {function(player,choice)
 			if business_id > 0 then -- check if not already have a shop
 				vRP.request(player, "Are you sure you want to rent this property?", 15, function(player,ok)
 					if ok then
 						vRP.withdrawBusiness(business_id,shop.rent,function(rowsChanged)
 							if rowsChanged > 0 then
 								-- bought, set address
+								shop.business = business_id
 								Log.write(user_id, "Rented "..shop.name.." for $"..shop.rent,Log.log_type.business)
 								vRP.logBusinessAction(business_id,user_id,user_id.." rented "..shop.name.." for $"..shop.rent)
 								vRPclient.notify(player,{"Your business has rented the shop. Be sure to keep it stocked!"})
+								vRP.closeMenu(player)
 							else
 								vRPclient.notify(player,{"Your business does not have the funds to cover this purchase."})
 							end
@@ -66,96 +68,100 @@ local function build_entry_menu(user_id, store_name)
 					end
 				end)
 			else
-				vRPclient.notify(player,"You must be employed by a business to rent a shop!")
+				vRPclient.notify(player,{"You must be employed by a business to rent a shop!"})
 			end
-		end)
-	end, "Rent this shop for $"..shop.rent}
+		end, "Rent this shop for $"..shop.rent}
+	elseif shop.business == business_id then
+		menu["Safe Withdraw"] = {function(player,choice)
+			vRP.request(player, "Withdraw all money from the safe?", 15, function(player,ok)
+				if ok then
+					-- sold, give sell price, remove address
+					Log.write(user_id, "Withdrew $"..shop.safe_money.." from "..shop.name,Log.log_type.business)
+					vRP.giveMoney(user_id, shop.safe_money)
+					vRPclient.notify(player,{"Withdrew $"..shop.safe_money.." from "..shop.name})
+					shop.safe_money = 0
+				end
+			end)
+		end, "Withdraw money from the safe ($"..shop.safe_money..")"}
 
-	menu["Safe Withdraw"] = {function(player,choice)
-		vRP.getPlayerBusiness(user_id,function(business_id)
-			if business_id ~= nil and business_id > 0 then -- check if already have a shop
-				vRP.request(player, "Sell Property", 15, function(player,ok)
-					if ok then
-						-- sold, give sell price, remove address
-						Log.write(user_id, "Sold a business for $"..shop.sell_price,Log.log_type.business)
-						vRP.giveMoney(user_id, shop.sell_price)
-						vRP.removeUserOffice(user_id)
-						vRPclient.notify(player,{lang.business.sell.sold()})
-					end
-				end)
-			else
-				vRPclient.notify(player,"You must be employed by a business to rent a shop!")
-			end
-		end)
-	end, lang.business.sell.description({shop.sell_price})}
-
-	local cb_add_inventory = function(idname)
-		local name,description,weight = vRP.getItemDefinition(idname)
-		local player = source
-		vRP.prompt(player, "Add how many "..name.." to stock?", "", function(player, p_input)
-			if parseInt(p_input) > 0 then
-				p_input = parseInt(p_input)
-				vRP.request(player, "Are you sure you want to stock "..p_input.." "..name, 30, function(hplayer,ok)
-					if ok then
-						if vRP.tryGetInventoryItem(user_id, idname, p_input, true) then
-							--Log.write(user_id,"Put "..amount.." "..vRP.getItemName(idname).." in "..chestname,Log.log_type.action)
-							cfg.stores[store_name].recipes[name].units = cfg.stores[store_name].recipes[name].units + p_input
-							vRP.setShopTransformer("cfg:"..store_name,cfg.stores[store_name])
-							-- actualize by closing
-							vRP.openMenu(player,menu)
+		local cb_add_inventory = function(idname)
+			local name,description,weight = vRP.getItemDefinition(idname)
+			local player = source
+			vRP.prompt(player, "Add how many "..name.." to stock?", "", function(player, p_input)
+				if parseInt(p_input) > 0 then
+					p_input = parseInt(p_input)
+					vRP.request(player, "Are you sure you want to stock "..p_input.." "..name, 30, function(hplayer,ok)
+						if ok then
+							if vRP.tryGetInventoryItem(user_id, idname, p_input, true) then
+								--Log.write(user_id,"Put "..amount.." "..vRP.getItemName(idname).." in "..chestname,Log.log_type.action)
+								cfg.stores[store_name].recipes[name].units = cfg.stores[store_name].recipes[name].units + p_input
+								vRP.setShopTransformer("cfg:"..store_name,cfg.stores[store_name])
+								-- actualize by closing
+								vRP.closeMenu(player)
+							end
 						end
-					end
-				end)
-			else
-				vRPclient.notify(player,{lang.common.invalid_value()})
-			end
-		end)
-	end
+					end)
+				else
+					vRPclient.notify(player,{lang.common.invalid_value()})
+				end
+			end)
+		end
 
-	local cb_create_listing = function(idname)
-		local name,description,weight = vRP.getItemDefinition(idname)
-		local player = source
-		vRP.prompt(player, "Item Price", "", function(player, p_input)
-			if parseInt(p_input) > 0 then
-				p_input = parseInt(p_input)
-				vRP.request(player, "Are you sure you want to offer "..name.." for $"..p_input, 30, function(hplayer,ok)
-					if ok then
-						cfg.stores[store_name].recipes[name] = {
-							description="Purchase "..name,
-							units=0,
-							in_money=p_input,
-							out_money=0,
-							reagents={},
-							products={
-								[idname] = 1
+		local cb_create_listing = function(idname)
+			local name,description,weight = vRP.getItemDefinition(idname)
+			local player = source
+			vRP.prompt(player, "Item Price", "", function(player, p_input)
+				if parseInt(p_input) > 0 then
+					p_input = parseInt(p_input)
+					vRP.request(player, "Are you sure you want to offer "..name.." for $"..p_input, 30, function(hplayer,ok)
+						if ok then
+							cfg.stores[store_name].recipes[name] = {
+								description="Purchase "..name,
+								units=0,
+								in_money=p_input,
+								out_money=0,
+								products={
+									[idname] = 1
+								}
 							}
-						}
-						vRP.setShopTransformer("cfg:"..store_name,cfg.stores[store_name])
-					end
-				end)
-			else
-				vRPclient.notify(player,{lang.common.invalid_value()})
+							vRP.setShopTransformer("cfg:"..store_name,cfg.stores[store_name])
+						end
+					end)
+				else
+					vRPclient.notify(player,{lang.common.invalid_value()})
+				end
+			end)
+		end
+
+		menu["Create Listing"] = {function(player,choice)
+			local data = vRP.getUserDataTable(user_id)
+			local submenu = build_itemlist_menu(lang.inventory.chest.put.title(), data.inventory, cb_create_listing)
+			submenu.onclose = function()
+				vRP.closeMenu(player)
 			end
-		end)
+			vRP.openMenu(player, submenu)
+		end, "List an item in your store"}
+
+		menu["Add Inventory"] = {function(player,choice)
+			local data = vRP.getUserDataTable(user_id)
+			local submenu = build_itemlist_menu(lang.inventory.chest.put.title(), data.inventory, cb_add_inventory)
+			submenu.onclose = function()
+				vRP.closeMenu(player)
+			end
+			vRP.openMenu(player, submenu)
+		end, "Add items to your store's inventory"}
 	end
-
-	menu["Create Listing"] = {function(player,choice)
-		local data = vRP.getUserDataTable(user_id)
-		local submenu = build_itemlist_menu(lang.inventory.chest.put.title(), data.inventory, cb_create_listing)
-		submenu.onclose = function()
-			vRP.closeMenu(player)
-		end
-		vRP.openMenu(player, submenu)
-	end, "List an item in your store"}
-
-	menu["Add Inventory"] = {function(player,choice)
-		local data = vRP.getUserDataTable(user_id)
-		local submenu = build_itemlist_menu(lang.inventory.chest.put.title(), data.inventory, cb_add_inventory)
-		submenu.onclose = function()
-			vRP.closeMenu(player)
-		end
-		vRP.openMenu(player, submenu)
-	end, "Add items to your store's inventory"}
+	
+	if shop.business == 0 or shop.business ~= business_id then
+		menu["Rob Store"] = {function(player,choice)
+			local data = vRP.getUserDataTable(user_id)
+			local submenu = build_itemlist_menu(lang.inventory.chest.put.title(), data.inventory, cb_add_inventory)
+			submenu.onclose = function()
+				vRP.closeMenu(player)
+			end
+			vRP.openMenu(player, submenu)
+		end, "Rob this poor shop of its hard earned money."}
+	end
 
 	return menu
 end
@@ -171,7 +177,9 @@ local function build_client_shops(source)
 			local function entry_enter(player,area)
 				local user_id = vRP.getUserId(player)
 				if user_id ~= nil and vRP.hasPermissions(user_id,v.permissions or {}) then
-					vRP.openMenu(source,build_entry_menu(user_id, k))
+					vRP.getPlayerBusiness(user_id, function(business_id)
+						vRP.openMenu(source,build_entry_menu(user_id, business_id, k))
+					end)
 				end
 			end
 
