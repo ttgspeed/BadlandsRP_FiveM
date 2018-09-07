4dd7908f481d704de9eb1fe1ce2cd599d674ea8c
Serpico <bamboozol@gmail.com>
2018-09-07 16:33:49 -0400

    Only give keys to vehicles you own

--- resources/vrp/client/vehicle_keys.lua
+++ resources/vrp/client/vehicle_keys.lua
@@ -2,8 +2,10 @@ local keys = {}
 
 function tvRP.giveKey(vehicleName, vehiclePlate)
   if vehicleName ~= nil and vehiclePlate ~= nil then
-    table.insert(keys, {name = string.lower(vehicleName), plate = string.lower(vehiclePlate)})
-    tvRP.notify("You received keys to a "..vehicleName.." with plate "..vehiclePlate)
+    if not tvRP.hasKey(vehicleName, vehiclePlate) then
+      table.insert(keys, {name = string.lower(vehicleName), plate = string.lower(vehiclePlate)})
+      tvRP.notify("You received keys to a "..vehicleName.." with plate "..vehiclePlate)
+    end
   end
 end
 
