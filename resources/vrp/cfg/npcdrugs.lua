72b9378f0fc1c90f133bde0aab4567403ea130a8
ttgspeed <justin@stricker.io>
2018-09-06 20:31:24 -0400

    Give only 20-30% drug money as dirty money. Remove cocaine dealer.

--- resources/vrp/cfg/npcdrugs.lua
+++ resources/vrp/cfg/npcdrugs.lua
@@ -1,6 +1,7 @@
 local cfg = {}
 
 cfg.drugs = {
+	["cocaine_pure"] = {lowPrice = 830, highPrice = 930, dirty = true, name = "Cocaine baggie", weight = 0.5},
 	["cocaine_poor"] = {lowPrice = 500, highPrice = 650, dirty = true, name = "Cocaine baggie", weight = 0.5},
 	["meth"] = {lowPrice = 260, highPrice = 375, dirty = true, name = "Meth baggie", weight = 0.5},
 	["weed"] = {lowPrice = 190, highPrice = 230, dirty = false, name = "Kifflom Kuff joint", weight = 0.5}, -- kifflom kush
