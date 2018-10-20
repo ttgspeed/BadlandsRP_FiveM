f453c5a699bf03d3aafa4935c358a99de51eebc7
Justin <ttg.speed@gmail.com>
2018-10-20 15:14:01 -0400

    Rename platinum to Titanium

--- resources/vrp/cfg/items.lua
+++ resources/vrp/cfg/items.lua
@@ -22,11 +22,11 @@ cfg.items = {
 
 	["raw_aluminum"] = {"Raw Aluminum", "A common metal. Needs refinement.", nil, 0.5},
 	["raw_steel"] = {"Raw Steel", "A strong metal. Needs refinement.", nil, 0.5},
-	["raw_platinum"] = {"Raw Platinum", "A valuable metal. Needs refinement.", nil, 0.5},
+	["raw_platinum"] = {"Raw Titanium", "A valuable metal. Needs refinement.", nil, 0.5},
 
 	["aluminum"] = {"Aluminum", "A common metal.", nil, 0.5},
 	["steel"] = {"Steel", "A strong metal.", nil, 0.5},
-	["platinum"] = {"Platinum", "A valuable metal.", nil, 0.5},
+	["titanium"] = {"Titanium", "A valuable metal.", nil, 0.5},
   --hunting
   ["rabbit_meat"] = {"Rabbit Meat", "Meat harvested from a poor, defenseless rabbit. You monster", nil, 0.5},
   ["boar_meat"] = {"Boar Meat", "Meat harvested from a boar", nil, 0.5},
