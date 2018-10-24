715b474600dc7fda6dd7447141270cdc6b35b855
Serpico <bamboozol@gmail.com>
2018-10-24 03:45:01 -0400

    added hospital interior at rockford hills with tp.

--- resources/maps/__resource.lua
+++ resources/maps/__resource.lua
@@ -1,3 +1,6 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
+
+file 'stream/hospital_props/int_medical.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/hospital_props/int_medical.ytyp'
 
 this_is_a_map 'yes'
