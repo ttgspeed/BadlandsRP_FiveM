21961dff4a1fb870d6a1e85bd5a4e8c449d890f5
Justin <ttg.speed@gmail.com>
2018-10-27 16:25:11 -0400

    Blast mine bug fixes

--- resources/vrp/modules/blast_mine.lua
+++ resources/vrp/modules/blast_mine.lua
@@ -1,4 +1,5 @@
 local Log = module("lib/Log")
+local lang = vRP.lang
 
 function tvRP.checkDynamite()
 	local task = TUNNEL_DELAYED()
@@ -25,12 +26,21 @@ end
 
 function tvRP.giveMetals(item,quantity)
 	local user_id = vRP.getUserId(source)
-	
+
+	local inventory_ok = true
+	local new_weight = vRP.getInventoryWeight(user_id)+vRP.getItemWeight(item)*quantity
+	if new_weight > vRP.getInventoryMaxWeight(user_id) then
+		inventory_ok = false
+		vRPclient.notify(source, {lang.inventory.full()})
+	end
+
 	if quantity > 10 then
 		Log.write(user_id, "Attempted to collect "..quantity.." "..item,Log.log_type.anticheat)
 		vRP.ban(user_id, user_id.." Scripting perm (serpickle)", 0)
 	end
 
-	Log.write(user_id, "Collected "..quantity.." "..item,Log.log_type.action)
-	vRP.giveInventoryItem(user_id,item,quantity,true)
+	if inventory_ok then
+		Log.write(user_id, "Collected "..quantity.." "..item,Log.log_type.action)
+		vRP.giveInventoryItem(user_id,item,quantity,true)
+	end
 end
