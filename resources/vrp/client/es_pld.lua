d82839a557a215d2b70fea9b5c000199afe1b1e0
Serpico <bamboozol@gmail.com>
2019-02-27 17:06:54 -0500

    talking anim hack

--- resources/vrp/client/es_pld.lua
+++ resources/vrp/client/es_pld.lua
@@ -380,3 +380,26 @@ end)
 function drawRct(x,y,width,height,r,g,b,a)
   DrawRect(x + width/2, y + height/2, width, height, r, g, b, a)
 end
+
+Citizen.CreateThread(function()
+    RequestAnimDict("facials@gen_male@variations@normal")
+    RequestAnimDict("mp_facial")
+
+    local talkingPlayers = {}
+    while true do
+        Citizen.Wait(300)
+
+        for k,v in pairs(GetPlayers()) do
+            local boolTalking = NetworkIsPlayerTalking(v)
+            if v ~= PlayerId() then
+                if boolTalking then
+                    PlayFacialAnim(GetPlayerPed(v), "mic_chatter", "mp_facial")
+                    talkingPlayers[v] = true
+                elseif not boolTalking and talkingPlayers[v] then
+                    PlayFacialAnim(GetPlayerPed(v), "mood_normal_1", "facials@gen_male@variations@normal")
+                    talkingPlayers[v] = nil
+                end
+            end
+        end
+    end
+end)
