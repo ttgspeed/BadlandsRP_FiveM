302f06e6ca061ad314fab06f7a1c96474489655c
Serpico <bamboozol@gmail.com>
2019-03-30 14:00:47 -0400

    revert and cleanup

--- resources/pQueue/server.lua
+++ resources/pQueue/server.lua
@@ -16,13 +16,10 @@ local slist = {}
 Config.Priority = {
 	-- Sr. Staff
 	["steam:11000010198b032"] = 100, --Serpico
-	["license:9dab3e051388782b38e3032a6c8b29f3945fb32c"] = 100, --Serpico
 	["steam:11000010268849f"] = 100, --speed
-	["license:1b979f4a93a0e21fd39c8f7d20d892a11ec5feb7"] = 100, --speed
 	-- Admins
 	["steam:110000100539323"] = 90, --0sk
 	["steam:11000010264f83b"] = 90, --Tiller
-	["steam:110000116047521"] = 90, --Tiller Alt
 	["steam:110000102c33401"] = 90, --Primal
 	["steam:1100001014f881e"] = 90, --Bob Lee
 	["steam:11000010a2cf14a"] = 90, --Morningstart
@@ -174,9 +171,6 @@ function Queue:IsPriority(ids, vrpId, isWhitelisted)
 		end
 
 		if self.Priority[v] then return self.Priority[v] ~= nil and self.Priority[v] or false end
-		if serverLabel == 'us1.blrp.life' or serverLabel == 'us2.blrp.life' then
-			if (vrpId < Config.MaxIDPriority) or isWhitelisted then return 1 or false end
-		end
 	end
 
 end
