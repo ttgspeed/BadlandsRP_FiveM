9149c6845b405dc68e04ebe627dbf6b38d7215c2
normpad <normpad48@gmail.com>
2019-04-19 03:17:00 -0400

    This fixes the cell phone prop

--- resources/gcphone/client/animation.lua
+++ resources/gcphone/client/animation.lua
@@ -53,7 +53,7 @@ function newPhoneProp()
 	while not HasModelLoaded(phoneModel) do
 		Citizen.Wait(1)
 	end
-	phoneProp = CreateObject(phoneModel, 1.0, 1.0, 1.0, 1, 1, 0)
+	phoneProp = CreateObject(GetHashKey(phoneModel), 1.0, 1.0, 1.0, true, true, true)
 	local bone = GetPedBoneIndex(myPedId, 28422)
 	AttachEntityToEntity(phoneProp, myPedId, bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2, 1)
 end
