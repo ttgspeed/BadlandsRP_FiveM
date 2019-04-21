0e23307cd9e2dba149f9e4cda2e63898c5061b2a
Justin <ttg.speed@gmail.com>
2019-04-21 14:36:38 -0400

    Disable cocaine lab power if too many people processing

--- resources/vrp/modules/cocaine.lua
+++ resources/vrp/modules/cocaine.lua
@@ -1,6 +1,16 @@
 local Log = module("lib/Log")
 
 local labPowerEnabled = true
+local playersProcessing = 0
+
+function tvRP.setPlayersProcessing(amount)
+	playersProcessing = playersProcessing + amount
+
+	if playersProcessing > 5 then
+		playersProcessing = 0
+		vRPclient.setCocaineLabPowerStatus(-1,{false})
+	end
+end
 
 function tvRP.hasCocaPasteIngredients()
 	local task = TUNNEL_DELAYED()
@@ -42,3 +52,10 @@ end
 function tvRP.broadcastCleanLab(status)
 	vRPclient.cleanCocaineLab(-1,{})
 end
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		print(playersProcessing)
+	end
+end)
