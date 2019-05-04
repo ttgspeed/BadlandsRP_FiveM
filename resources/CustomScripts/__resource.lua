ba7f713b6d68686477b695605d01218fb39dc2f7
Serpico <bamboozol@gmail.com>
2019-05-03 20:22:19 -0400

    [UNTESTED] Added ability to push a vehicle when disabled. Can push if flipped. Health needs to be < 25.

--- resources/CustomScripts/__resource.lua
+++ resources/CustomScripts/__resource.lua
@@ -15,6 +15,7 @@ client_scripts{
 	"client/carhud.lua",
 	"client/GunDraw.lua",
 	"client/carwash.lua",
+	"client/pushvehicle.lua",
 	"RealisticVehicleFailure/config.lua",
 	"RealisticVehicleFailure/client.lua",
 }
