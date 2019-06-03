9a91b492c5131a5b59a2d56d1cf21d7834b97571
Serpico <bamboozol@gmail.com>
2019-06-02 22:16:56 -0400

    wrong loader

--- resources/addons/__resource.lua
+++ resources/addons/__resource.lua
@@ -10,7 +10,7 @@ files {
 	'vehicleaihandlinginfo.meta',
 	'weapons.meta',
 	'weaponanimations.meta',
-	'damages.meta',
+	--'damages.meta',
 	--'combatbehaviour.meta',
 	'results.meta',
 	'pedaccuracy.meta',
@@ -57,7 +57,7 @@ data_file 'WEAPONINFO_FILE_PATCH' 'weaponpoolcue.meta'
 data_file 'WEAPONINFO_FILE_PATCH' 'weaponswitchblade.meta'
 data_file 'WEAPONINFO_FILE_PATCH' 'weaponwrench.meta'
 data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
-data_file 'PED_DAMAGE_APPEND_FILE' 'damages.meta'
+--data_file 'PED_DAMAGE_APPEND_FILE' 'damages.meta'
 --data_file 'FIVEM_LOVES_YOU_1424B0DA6F9BC846' 'combatbehaviour.meta'
 data_file 'PED_DAMAGE_APPEND_FILE' 'results.meta'
 data_file 'WEAPONINFO_FILE_PATCH' 'pedaccuracy.meta'
