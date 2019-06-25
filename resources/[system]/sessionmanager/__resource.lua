249ae3111c85059a65d049dd65be32211ededd33
Serpico <bamboozol@gmail.com>
2019-06-25 01:30:29 -0400

    Possible fix for sessionmanager errors, even though its not loaded

--- resources/[system]/sessionmanager/__resource.lua
+++ resources/[system]/sessionmanager/__resource.lua
@@ -1,3 +1,4 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 server_script 'server/host_lock.lua'
+client_script 'client/empty.lua'
