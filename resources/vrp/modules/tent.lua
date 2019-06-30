16b07c48af4380e8b4028a75da3da4465fb0cf3a
Justin <ttg.speed@gmail.com>
2019-06-30 17:13:29 -0400

    Tent storage

--- resources/vrp/modules/tent.lua
+++ resources/vrp/modules/tent.lua
@@ -35,45 +35,50 @@ function vRP.removeUserTent(user_id)
 end
 
 local function tent_leave(player,area)
-    vRP.closeMenu(player)
+	vRP.closeMenu(player)
 end
 
 local function tent_enter(player,area)
 	local user_id = vRP.getUserId(player)
-    print(area)
-    local tent_owner = tent_area_owners[area]
-    print(tent_owner)
+	local tent_owner = tent_area_owners[area]
 	if user_id ~= nil then
 		vRPclient.isPedInCar(player, {}, function(inVeh)
 			if not inVeh then
-                vRP.getUserTent(user_id, function(e_tent)
-            		if e_tent ~= nil then
-                        local menu = {name="Tent",css={top="75px",header_color="rgba(255,125,24,0.75)"}}
-        				menu["Tent"] = {function(player,choice)
-                            print("tent owned by "..tent_owner)
-                            vRP.prompt(player, lang.home.gametable.bet.prompt(), "", function(player, pin)
-                				pin = parseInt(pin)
-                				if pin > 0 then
-                                    vRP.openChest(player, "u"..tent_owner.."tent", config.weight or 150,nil,nil,nil)
-                                end
-                            end)
-                        end, "Do tent things"}
-                        menu["Tent2"] = {function(player,choice) end, "Do tent things"}
+                print(tent_owner)
+				vRP.getUserTent(tent_owner, function(e_tent)
+					if e_tent ~= nil then
+                        local e_tent = json.decode(e_tent.data)
 
-                        vRP.openMenu(player,menu)
-                    end
-                end)
+						local menu = {name="Tent",css={top="75px",header_color="rgba(255,125,24,0.75)"}}
+						menu["Lockbox"] = {function(player,choice)
+							vRP.prompt(player,"Enter a lock combination (0000-9999)","",function(player,lock)
+                                print(e_tent.lock)
+                                print(lock)
+								if e_tent.lock ~= nil and e_tent.lock == lock then
+									local chestname = "u"..tent_owner.."tent"
+								    local max_weight = 150 --cfg_inventory.vehicle_chest_weights[string.lower(name)] or 150
+								    -- open chest
+								    vRP.openChest(player, chestname, max_weight)
+								end
+							end)
+						end, "Open the tent's lockbox",1}
+						menu["Break lock"] = {function(player,choice) end, "Break the lockbox's lock.",2}
+                        menu["Duffel bag"] = {function(player,choice) end, "Break the lockbox's lock.",3}
+
+						vRP.openMenu(player,menu)
+					end
+				end)
 			end
 		end)
 	end
 end
 
 local function createTentArea(tent_owner,tent_pos)
-    for _,player in pairs(GetPlayers()) do
-        local x,y,z = table.unpack(tent_pos)
-        vRP.setArea(player,"vRP:tent:"..tent_owner,x,y,z,2,2,tent_enter,tent_leave)
-        tent_area_owners["vRP:tent:"..tent_owner] = tent_owner
-    end
+	for _,player in pairs(GetPlayers()) do
+		local x,y,z = table.unpack(tent_pos)
+		vRP.setArea(player,"vRP:tent:"..tent_owner,x,y,z,3,3,tent_enter,tent_leave)
+		tent_area_owners["vRP:tent:"..tent_owner] = tent_owner
+	end
 end
 
 -- access a home by address
@@ -83,20 +88,27 @@ function vRP.createTent(player)
 	local player = player
 	vRP.getUserTent(user_id, function(e_tent)
 		if e_tent == nil then
-			vRPclient.getForwardPosition(player,{},function(x,y,z)
-				if z > 0 then
-					if vRP.tryGetInventoryItem(user_id,"tent",1,false) then
-						local tent = {
-							["pos"] = {x,y,z},
-							["inventory"] = {}
-						}
-						active_tents[user_id] = tent
-						vRP.addUserTent(user_id,json.encode(tent))
-						vRPclient.addTent(-1,{user_id,tent.pos})
-                        createTentArea(user_id,tent.pos)
+			vRP.prompt(player,"Enter a lock combination (0000-9999)","",function(player,lock)
+				if string.len(lock) == 4 then
+					lock_int = tonumber(lock)
+					if lock_int >= 0 and lock_int <= 9999 then
+						vRPclient.getForwardPosition(player,{},function(x,y,z)
+							if z > 0 then
+								if vRP.tryGetInventoryItem(user_id,"tent",1,false) then
+									local tent = {
+										["pos"] = {x,y,z},
+										["lock"] = lock
+									}
+									active_tents[user_id] = tent
+									vRP.addUserTent(user_id,json.encode(tent))
+									vRPclient.addTent(-1,{user_id,tent.pos})
+									createTentArea(user_id,tent.pos)
+								end
+							else
+								vRPclient.notify(player,{"Unable to place tent here. Please try again."})
+							end
+						end)
 					end
-				else
-					vRPclient.notify(player,{"Unable to place tent here. Please try again."})
 				end
 			end)
 		else
@@ -110,8 +122,8 @@ AddEventHandler("vRP:playerSpawn",function(user_id, source, first_spawn)
 		vRP.getUserTent(user_id, function(e_tent)
 			if e_tent ~= nil then
 				local e_tent = json.decode(e_tent.data)
-                createTentArea(user_id,e_tent.pos)
-                vRPclient.addTent(-1,{user_id,e_tent.pos})
+				createTentArea(user_id,e_tent.pos)
+				vRPclient.addTent(-1,{user_id,e_tent.pos})
 				for k,v in pairs(active_tents) do
 					if v ~= nil then
 						vRPclient.addTent(player,{user_id,v.pos})
