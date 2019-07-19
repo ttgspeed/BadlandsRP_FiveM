923dd0c74de50dbea65f89ef180f21807efc7a3f
Justin <ttg.speed@gmail.com>
2019-07-19 18:13:16 -0400

    Transition tent robbery to generic robbery handler. Store robbery/raid will use this.

--- resources/vrp/client/tent.lua
+++ resources/vrp/client/tent.lua
@@ -38,39 +38,6 @@ function tvRP.getForwardPosition()
     return x,y,z
 end
 
-function tvRP.startTentRobbery(owner, location)
-    local robbery_time_remaining = 600
-    local robbery_done = false
-    Citizen.CreateThread(function()
-        while robbery_done == false do
-            Citizen.Wait(1000)
-            local ped = GetPlayerPed(-1)
-            local x,y,z = table.unpack(GetEntityCoords(ped,true))
-            local tx,ty,tz = table.unpack(location)
-
-            if GetDistanceBetweenCoords(x,y,z,tx,ty,tz,true) > 3.0 or tvRP.isHandcuffed() or tvRP.isInComa() or IsPedInAnyVehicle(ped) then
-                --fail
-                vRPserver.resolveTentRobbery({owner,false})
-                robbery_done = true
-            end
-
-            if robbery_time_remaining == 0 then
-                --success
-                vRPserver.resolveTentRobbery({owner,true})
-                robbery_done = true
-            else
-                robbery_time_remaining = robbery_time_remaining - 1
-            end
-        end
-    end)
-    Citizen.CreateThread(function()
-        while robbery_done == false do
-            Citizen.Wait(0)
-            tvRP.missionText("Breaking lock: ~r~" .. robbery_time_remaining .. "~w~ seconds remaining")
-        end
-    end)
-end
-
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1000)
