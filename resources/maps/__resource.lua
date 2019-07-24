684f7ba1f76dee9aff5063f50d5ca7f66d32d54f
Serpico <bamboozol@gmail.com>
2019-07-24 03:12:10 -0400

    Fill hospital props

--- resources/maps/__resource.lua
+++ resources/maps/__resource.lua
@@ -2,6 +2,13 @@ resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
 
 this_is_a_map 'yes'
 
+files {
+  'stream/hostpital_props/hoint_medical.ytyp',
+  'stream/hostpital_props/_manifest.ymf'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/hostpital_props/int_medical.ytyp'
+
 client_scripts {
     "client/main.lua"
 }
