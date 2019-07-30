4f35f4e98aa2c49490102c25e4324662c902491c
Serpico <bamboozol@gmail.com>
2019-07-30 02:34:40 -0400

    prevent ai spanw in lower mission row

--- resources/maps/client/main.lua
+++ resources/maps/client/main.lua
@@ -4,7 +4,8 @@ LoadInterior(Interior)
 
 Citizen.CreateThread(function()
     while (true) do
-        ClearAreaOfPeds(342.44, -587.00, 27.79, 50.0, 1) -- clear peds of pilbox hospital
-        Citizen.Wait(0)
+      Citizen.Wait(0)
+      ClearAreaOfPeds(342.44, -587.00, 27.79, 50.0, 1) -- clear peds of pilbox hospital
+      ClearAreaOfPeds(470.64236450195,-990.66455078125,24.914754867554, 25.0, 1) -- clear peds of mission row
     end
 end)
