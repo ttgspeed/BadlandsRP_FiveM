6a0263d4082161e22a6e40a7683c6b5b0f6bc137
Serpico <bamboozol@gmail.com>
2019-09-03 02:16:29 -0400

    Sandy and paleto ems interiors

--- resources/LowerPillbox/__resource.lua
+++ resources/LowerPillbox/__resource.lua
@@ -1,2 +1,11 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
 this_is_a_map 'yes'
+
+file 'int_medical.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'int_medical.ytyp'
+
+file 'builderdefaddons.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'builderdefaddons.ytyp'
+
+client_script  'client.lua'
