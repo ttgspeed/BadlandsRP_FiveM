9ff07be85b87220865bd63155110fce49ed294dd
Serpico <bamboozol@gmail.com>
2019-09-10 04:22:44 -0400

    remove body scroll bar. disable anim while in veh (might be able to fix)

--- resources/blrp_mdt/client.lua
+++ resources/blrp_mdt/client.lua
@@ -10,33 +10,35 @@ end
 local tabletEntity = nil
 
 function tabletAnim(boolean)
-	if boolean then
-		loadModels(GetHashKey("prop_cs_tablet"))
+  if not IsPedInAnyVehicle(GetPlayerPed(-1)) then
+  	if boolean then
+  		loadModels(GetHashKey("prop_cs_tablet"))
 
-		tabletEntity = CreateObject(GetHashKey("prop_cs_tablet"), GetEntityCoords(PlayerPedId()), true)
+  		tabletEntity = CreateObject(GetHashKey("prop_cs_tablet"), GetEntityCoords(PlayerPedId()), true)
 
-		AttachEntityToEntity(tabletEntity, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 28422), -0.03, 0.0, 0.0, 0.0, 0.0, 0.0, true, true, false, true, 1, true)
+  		AttachEntityToEntity(tabletEntity, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 28422), -0.03, 0.0, 0.0, 0.0, 0.0, 0.0, true, true, false, true, 1, true)
 
-    loadModels("amb@code_human_in_bus_passenger_idles@female@tablet@idle_a")
+      loadModels("amb@code_human_in_bus_passenger_idles@female@tablet@idle_a")
 
-		TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@idle_a", "idle_a", 3.0, -8, -1, 63, 0, 0, 0, 0 )
+  		TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@idle_a", "idle_a", 3.0, -8, -1, 63, 0, 0, 0, 0 )
 
-		Citizen.CreateThread(function()
-			while DoesEntityExist(tabletEntity) do
-				Citizen.Wait(5)
+  		Citizen.CreateThread(function()
+  			while DoesEntityExist(tabletEntity) do
+  				Citizen.Wait(5)
 
-				if not IsEntityPlayingAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@idle_a", "idle_a", 3) then
-					TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@idle_a", "idle_a", 3.0, -8, -1, 63, 0, 0, 0, 0 )
-				end
-			end
+  				if not IsEntityPlayingAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@idle_a", "idle_a", 3) then
+  					TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@idle_a", "idle_a", 3.0, -8, -1, 63, 0, 0, 0, 0 )
+  				end
+  			end
 
-			ClearPedTasks(PlayerPedId())
-		end)
-	else
-    if tabletEntity ~= nil then
-      DeleteEntity(tabletEntity)
-    end
-	end
+  			ClearPedTasks(PlayerPedId())
+  		end)
+  	else
+      if tabletEntity ~= nil then
+        DeleteEntity(tabletEntity)
+      end
+  	end
+  end
 end
 
 function loadModels(model)
