a727fb1fc834089c557e707c3cf603751369ba0a
Serpico <bamboozol@gmail.com>
2019-10-03 01:33:14 -0400

    Closest item helper functions

--- resources/vrp/lib/utils.lua
+++ resources/vrp/lib/utils.lua
@@ -30,13 +30,13 @@ end
 -- generate a task metatable (helper to return delayed values with timeout)
 --- dparams: default params in case of timeout or empty cbr()
 --- timeout: milliseconds, default 5000
-function Task(callback, dparams, timeout) 
+function Task(callback, dparams, timeout)
   if timeout == nil then timeout = 5000 end
 
   local r = {}
   r.done = false
 
-  local finish = function(params) 
+  local finish = function(params)
     if not r.done then
       if params == nil then params = dparams or {} end
       r.done = true
@@ -53,7 +53,7 @@ end
 function parseInt(v)
 --  return cast(int,tonumber(v))
   local n = tonumber(v)
-  if n == nil then 
+  if n == nil then
     return 0
   else
     return math.floor(n)
@@ -129,4 +129,97 @@ function joinStrings(list, sep)
   end
 
   return str
+end
+
+function GetClosestPed(radius)
+    local closestPed = 0
+
+    for ped in EnumeratePeds() do
+        local distanceCheck = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), GetEntityCoords(ped), true)
+        if distanceCheck <= radius+.000001 and ped ~= GetPlayerPed(-1) then
+            closestPed = ped
+            break
+        end
+    end
+
+    return closestPed
+end
+
+function GetClosestVehicle(radius)
+    local closestVeh = 0
+
+    for veh in EnumerateVehicles() do
+        local distanceCheck = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), GetEntityCoords(veh), true)
+        if distanceCheck <= radius+.000001 then
+            closestVeh = veh
+            break
+        end
+    end
+
+    return closestVeh
+end
+
+function GetClosestParkedVehicles(radius, maxQty)
+	local closestVehicles = {}
+	local count = 0
+	for veh in EnumerateVehicles() do
+		local distanceCheck = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), GetEntityCoords(veh), true)
+		local popType = GetEntityPopulationType(veh)
+		if distanceCheck <= radius+.000001 and popType == 2 then
+			count = count + 1
+			table.insert(closestVehicles, veh)
+		end
+		if count == maxQty then
+			break
+		end
+	end
+	return closestVehicles
+end
+
+local entityEnumerator = {
+  __gc = function(enum)
+    if enum.destructor and enum.handle then
+      enum.destructor(enum.handle)
+    end
+    enum.destructor = nil
+    enum.handle = nil
+  end
+}
+
+local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+  return coroutine.wrap(function()
+    local iter, id = initFunc()
+    if not id or id == 0 then
+      disposeFunc(iter)
+      return
+    end
+
+    local enum = {handle = iter, destructor = disposeFunc}
+    setmetatable(enum, entityEnumerator)
+
+    local next = true
+    repeat
+      coroutine.yield(id)
+      next, id = moveFunc(iter)
+    until not next
+
+    enum.destructor, enum.handle = nil, nil
+    disposeFunc(iter)
+  end)
+end
+
+function EnumerateObjects()
+  return EnumerateEntities(FindFirstObject, FindNextObject, EndFindObject)
+end
+
+function EnumeratePeds()
+  return EnumerateEntities(FindFirstPed, FindNextPed, EndFindPed)
+end
+
+function EnumerateVehicles()
+  return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
+end
+
+function EnumeratePickups()
+  return EnumerateEntities(FindFirstPickup, FindNextPickup, EndFindPickup)
 end
